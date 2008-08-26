f7a8e336c1e5d520d4dca90989d138270cffff8b
Sebastian Walter <walter@mathematik.hu-berlin.de>
2008-08-26 09:21:06 +0200

    edge list graph, this graph structure is not suitable

--- reverse_mode.py
+++ reverse_mode.py
@@ -9,6 +9,10 @@ class CGraph:
 	vertexNumberCount = 0
 	edgeNumberCount = 0
 	vertexList = []
+	edgeList = []
+
+	def __str__(self):
+		return 'vertices:\n' + str(CGraph.vertexList) +'\nedges:\n'+ str(CGraph.edgeList)
 
 	class Vertex:
 		def __init__(self, vertex_type):
@@ -29,8 +33,7 @@ class CGraph:
 
 			self.id = CGraph.vertexNumberCount
 			CGraph.vertexNumberCount += 1
-			self.out_edges = []
-			self.in_edges = []
+
 
 		def __str__(self):
 			return 'v%s'%(str(self.id))
@@ -42,6 +45,7 @@ class CGraph:
 		def __init__(self,source, target):
 			self.source = source
 			self.target = target
+			CGraph.edgeList.append(self)
 			
 		def __str__(self):
 			return '[%s,%s]'%(str(self.source), str(self.target))
@@ -60,66 +64,59 @@ class adouble:
 		retval = adouble(self.x + rhs.x, self.dx + rhs.dx)
 		
 		f = CGraph.Vertex(vertex_type='add')
-		e3 = CGraph.Edge(f,retval.vertex)
-		retval.vertex.in_edges.append(e3)
-		f.out_edges.append(e3)
+		CGraph.Edge(f,retval.vertex)
 		
 		CGraph.vertexList.append(f)
 		e1 = CGraph.Edge(self.vertex,f)
 		e2 = CGraph.Edge(rhs.vertex,f)
-		f.in_edges.append(e1)
-		f.in_edges.append(e2)
 
-		self.vertex.out_edges.append(e1)
-		rhs.vertex.out_edges.append(e2)
 		return retval
 
 	def __mul__(self,rhs):
 		retval = adouble(self.x * rhs.x, self.dx * rhs.x + self.x * rhs.dx)
 		
 		f = CGraph.Vertex(vertex_type='mul')
-		e3 = CGraph.Edge(f,retval.vertex)
-		retval.vertex.in_edges.append(e3)
-		f.out_edges.append(e3)
+		CGraph.Edge(f,retval.vertex)
 		
 		CGraph.vertexList.append(f)
 		e1 = CGraph.Edge(self.vertex,f)
 		e2 = CGraph.Edge(rhs.vertex,f)
-		f.in_edges.append(e1)
-		f.in_edges.append(e2)
 
-		self.vertex.out_edges.append(e1)
-		rhs.vertex.out_edges.append(e2)
 		return retval
 
 # tape operations
 ax = adouble(1.,1.)
 ay = adouble(2.)
-az = ax*ay + ax*ay
+az = ax*ay + ax*ay + ay
 
 # show result of the forward evaluation
 print 'd/dx ( 2 * x * y) = ', az.dx
 
-# compute reverse sweep
-ubar = 1.
-for v in CGraph.vertexList[-1::-1]:
-	for e in v.in_edges:
+# reverse evaluation
+CGraph.edgeList[-1].target.xbar = 1.
+for e in CGraph.edgeList[-1::-1]:
+	if e.target.type == 'add':
+		e.source.xbar = e.target.xbar
+	elif e.target.type == 'mul':
+		e.source.xbar = e.target.xbar
 
 
-from pygraphviz import *
+##print graph
+#print CGraph()
 
-A = AGraph()
-A.node_attr['style']='filled'
-A.node_attr['shape']='circle'
-A.node_attr['fixedsize']='true'
-A.node_attr['fontcolor']='#000000'
+#from pygraphviz import *
 
-#for v in CGraph.vertexList:
-	#for e in v.out_edges:
-		#print v.id,'->',e.target.id,"  :   ",v,'->',e.target
-		#A.add_edge(v.id,e.target.id)
-		#s = A.get_node(v.id)
-		#if v.type == 'add':
+#A = AGraph()
+#A.node_attr['style']='filled'
+#A.node_attr['shape']='circle'
+#A.node_attr['fixedsize']='true'
+#A.node_attr['fontcolor']='#000000'
+
+#for e in CGraph.edgeList:
+	#A.add_edge(e.source.id, e.target.id)
+	#s = A.get_node(e.source.id)
+	#vtype = e.source.type
+	#if vtype == 'add':
 			#s.attr['fillcolor']="#000000"
 			#s.attr['shape']='rect'
 			#s.attr['label']='+'
@@ -127,20 +124,20 @@ A.node_attr['fontcolor']='#000000'
 			#s.attr['height']='0.3'
 			#s.attr['fontcolor']='#ffffff'
 
-		#elif v.type == 'mul':
-			#s.attr['fillcolor']="#000000"
-			#s.attr['shape']='rect'
-			#s.attr['label']='*'
-			#s.attr['width']='0.3'
-			#s.attr['height']='0.3'
-			#s.attr['fontcolor']='#ffffff'
+	#elif vtype == 'mul':
+		#s.attr['fillcolor']="#000000"
+		#s.attr['shape']='rect'
+		#s.attr['label']='*'
+		#s.attr['width']='0.3'
+		#s.attr['height']='0.3'
+		#s.attr['fontcolor']='#ffffff'
 
-		#elif v.type == 'v':
-			#s.attr['fillcolor']="#FFFF00"
-			#s.attr['shape']='circle'
-			#s.attr['label']= 'v_%d'%v.id
-			#s.attr['width']='0.3'
-			#s.attr['height']='0.3'
+	#elif vtype == 'v':
+		#s.attr['fillcolor']="#FFFF00"
+		#s.attr['shape']='circle'
+		#s.attr['label']= 'v_%d'%e.source.id
+		#s.attr['width']='0.3'
+		#s.attr['height']='0.3'
 
 #print A.string() # print to screen
 
@@ -148,43 +145,4 @@ A.node_attr['fontcolor']='#000000'
 #import os
 #os.system("dot  computational_graph.dot -Tsvg -o computational_graph.svg")
 #os.system("dot  computational_graph.dot -Tpng -o computational_graph.png")
-
-
-
-for v in CGraph.vertexList[-1::-1]:
-	for e in v.in_edges:
-		print v.id,'->',e.source.id,"  :   ",v,'->',e.source
-		A.add_edge(v.id,e.source.id)
-		s = A.get_node(v.id)
-		if v.type == 'add':
-			s.attr['fillcolor']="#000000"
-			s.attr['shape']='rect'
-			s.attr['label']='+'
-			s.attr['width']='0.3'
-			s.attr['height']='0.3'
-			s.attr['fontcolor']='#ffffff'
-
-		elif v.type == 'mul':
-			s.attr['fillcolor']="#000000"
-			s.attr['shape']='rect'
-			s.attr['label']='*'
-			s.attr['width']='0.3'
-			s.attr['height']='0.3'
-			s.attr['fontcolor']='#ffffff'
-
-		elif v.type == 'v':
-			s.attr['fillcolor']="#FFFF00"
-			s.attr['shape']='circle'
-			s.attr['label']= 'v_%d'%v.id
-			s.attr['width']='0.3'
-			s.attr['height']='0.3'
-
-print A.string() # print to screen
-
-A.write("rev_computational_graph.dot")
-import os
-os.system("dot  rev_computational_graph.dot -Tsvg -o rev_computational_graph.svg")
-os.system("dot  rev_computational_graph.dot -Tpng -o rev_computational_graph.png")
-
-
 
