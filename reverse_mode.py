aa783acb00c7720df4c9e65dbcdb609ceb11ea5f
Sebastian Walter <walter@mathematik.hu-berlin.de>
2008-10-24 19:56:24 +0200

    working first order derivatives (gradient)

--- reverse_mode.py
+++ reverse_mode.py
@@ -5,17 +5,26 @@ from numpy import *
 import numpy
 
 class Tc:
-	def __init__(self,x):
-		self.x = x
+	def __init__(self,tc):
+		self.tc = asarray(tc)
 
 	def __add__(self,rhs):
-		return Tc([ self.x[0] + rhs.x[0], self.x[1] + rhs.x[1] ])
+		return Tc(self.tc[:] + rhs.tc[:])
 	
 	def __mul__(self,rhs):
-		return Tc([ self.x[0] * rhs.x[0], self.x[0]*rhs.x[1] + self.x[1]*rhs.x[0] ])
+		D = size(self.tc)
+		E = size(rhs.tc)
+		assert D==E
+		assert size(self.tc)>0
+		assert size(rhs.tc)>0
+
+		return Tc(npy.array(
+					[ npy.sum(self.tc[:k+1] * rhs.tc[k::-1], axis = 0) for k in range(D)]
+					))
+
 
 	def __str__(self):
-		return str(self.x)
+		return str(self.tc)
 
 class Function:
 	def __init__(self, args, function_type='var'):
@@ -32,26 +41,37 @@ class Function:
 
 		self.args = args
 		self.x = self.eval()
-		self.xbar = Tc([0.,0.])
+		self.xbar = Tc(zeros(size(self.x)))
 		self.id = self.cgraph.functionCount
 		self.cgraph.functionCount += 1
 		self.cgraph.functionList.append(self)
 
 	def __add__(self,rhs):
 		if not isinstance(rhs, Function):
-			rhs = Function(rhs)
+			a = zeros(size(self.x.tc))
+			a[0] = rhs
+			rhs = Function(a)
 		return Function([self, rhs], function_type='add')
 
 	def __mul__(self,rhs):
 		if not isinstance(rhs, Function):
-			rhs = Function(rhs)
+			a = zeros(size(self.x.tc))
+			a[0] = rhs
+			rhs = Function(a)
 		return Function([self, rhs], function_type='mul')	
 
 	def __radd__(self,lhs):
 		return self + lhs
+
+	def __rmul__(self,lhs):
+		return self * lhs
 	
 	def __str__(self):
-		return '%s%s:(%s,%s)'%(self.type,str(self.id),str(self.x), str(self.xbar))
+		try:
+			ret = '%s%s:(x=%s)(xbar=%s)'%(self.type,str(self.id),str(self.x),str(self.xbar))
+		except:
+			ret = '%s%s:(x=%s)'%(self.type,str(self.id),str(self.x))
+		return ret
 	
 	def __repr__(self):
 		return self.__str__()
@@ -61,7 +81,7 @@ class Function:
 			return Tc(self.args)
 		
 		elif self.type == 'add':
-			return self.args[0].x + self.args[1].x #[self.args[0].x[0] + self.args[1].x[0], self.args[0].x[1] + self.args[1].x[1]]
+			return self.args[0].x + self.args[1].x
 
 		elif self.type == 'mul':
 			return self.args[0].x * self.args[1].x
@@ -69,14 +89,15 @@ class Function:
 	def reval(self):
 		if self.type == 'var':
 			pass
-		
+
+
 		elif self.type == 'add':
-			self.args[0].xbar = self.xbar
-			self.args[1].xbar = self.xbar
+			self.args[0].xbar += self.xbar
+			self.args[1].xbar += self.xbar
 
 		elif self.type == 'mul':
-			self.args[0].xbar = self.xbar * self.args[1].x
-			self.args[1].xbar = self.xbar * self.args[0].x
+			self.args[0].xbar += self.xbar * self.args[1].x
+			self.args[1].xbar += self.xbar * self.args[0].x
 
 
 class CGraph:
@@ -124,26 +145,26 @@ class CGraph:
 		for f in self.functionList[::-1]:
 			f.reval()
 
-#def tape(f,in_x):
-	#x = in_x[:]
-	#N = len(x)
-	#cg = CGraph()
-	#ax = numpy.array([Function(x[n]) for n in range(N)])
-	#cg.independentFunctionList = ax
-	#ay = f(ax)
-	#cg.dependentFunctionList = numpy.array([ay])
-	#return cg
+def tape(f,in_x):
+	x = in_x.copy()
+	N = size(x)
+	cg = CGraph()
+	ax = numpy.array([Function([x[n]]) for n in range(N)])
+	cg.independentFunctionList = ax
+	ay = f(ax)
+	cg.dependentFunctionList = numpy.array([ay])
+	return cg
 
-#def gradient_from_graph(cg,x=None):
-	#if x != None:
-		#cg.forward(x)
-	#cg.reverse(numpy.array([1.]))
-	#N = len(cg.independentFunctionList)
-	#return numpy.array([cg.independentFunctionList[n].xbar for n in range(N)])
+def gradient_from_graph(cg,x=None):
+	if x != None:
+		cg.forward(x)
+	cg.reverse(numpy.array([Tc([1.])]))
+	N = size(cg.independentFunctionList)
+	return numpy.array([cg.independentFunctionList[n].xbar.tc[0] for n in range(N)])
 
-#def gradient(f, in_x):
-	#cg = tape(f,in_x)
-	#return gradient_from_graph(cg)
+def gradient(f, in_x):
+	cg = tape(f,in_x)
+	return gradient_from_graph(cg)
 
 
 if __name__ == "__main__":
@@ -158,29 +179,38 @@ if __name__ == "__main__":
 	cg.reverse([Tc([1.,0.])])
 	print x.xbar
 	print y.xbar
-	#import numpy
-	#import numpy.linalg
-	#N = 10
-	#A = 0.125 * numpy.eye(N)
-	#x = numpy.ones(N)
 
-	#def f(x):
-		#return 0.5*numpy.dot(x, numpy.dot(A,x))
+	def f(x):
+		return x[0]*x[1] + 2323
+	x = array([11.,13.])
+	cg = tape(f,x)
+	print cg
+	gradient_from_graph(cg)
+	#print cg
+	print gradient(f,x)
 
 
-	## normal function evaluation
-	#y = f(x)
+	N = 10
+	A = 0.125 * numpy.eye(N)
+	x = numpy.ones(N)
 
-	## taping + reverse evaluation
-	#g_reverse = gradient(f,x)
+	def f(x):
+		return 0.5*numpy.dot(x, numpy.dot(A,x))
 
-	#print g_reverse
 
-	## taping
-	#cg = tape(f,x)
-	#print gradient_from_graph(cg)
+	# normal function evaluation
+	y = f(x)
 
-	#cg.plot()
+	# taping + reverse evaluation
+	g_reverse = gradient(f,x)
+
+	print g_reverse
+
+	#taping
+	cg = tape(f,x)
+	print gradient_from_graph(cg)
+
+	##cg.plot()
 
 
 
