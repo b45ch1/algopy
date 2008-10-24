f8e14763d225116f12edd50cad1258a213eeca7b
Sebastian Walter <walter@mathematik.hu-berlin.de>
2008-10-24 15:05:23 +0200

    new improved reverse mode: can compute now second order derivatives

--- reverse_mode.py
+++ reverse_mode.py
@@ -4,25 +4,35 @@ from pylab import *
 from numpy import *
 import numpy
 
+class Tc:
+	def __init__(self,x):
+		self.x = x
+
+	def __add__(self,rhs):
+		return Tc([ self.x[0] + rhs.x[0], self.x[1] + rhs.x[1] ])
+	
+	def __mul__(self,rhs):
+		return Tc([ self.x[0] * rhs.x[0], self.x[0]*rhs.x[1] + self.x[1]*rhs.x[0] ])
+
+	def __str__(self):
+		return str(self.x)
+
 class Function:
 	def __init__(self, args, function_type='var'):
 		if function_type == 'var':
 			self.type = 'var'
-			self.args = [Arg(args, source = None)]
 		elif function_type == 'id':
 			self.type = 'id'
-			self.args = args
 		elif function_type == 'add':
 			self.type = 'add'
-			self.args = args
 		elif function_type == 'mul':
 			self.type = 'mul'
-			self.args = args
 		else:
 			raise NotImplementedError('function_type must be either \'v\' or \'mul\' or  \'add\'')
 
-
-		self.xbar = 0.
+		self.args = args
+		self.x = self.eval()
+		self.xbar = Tc([0.,0.])
 		self.id = self.cgraph.functionCount
 		self.cgraph.functionCount += 1
 		self.cgraph.functionList.append(self)
@@ -30,59 +40,44 @@ class Function:
 	def __add__(self,rhs):
 		if not isinstance(rhs, Function):
 			rhs = Function(rhs)
-		left_adouble = Arg(self.eval(),self)
-		right_adouble = Arg(rhs.eval(), rhs)
-		return Function([left_adouble, right_adouble], function_type='add')
+		return Function([self, rhs], function_type='add')
 
 	def __mul__(self,rhs):
 		if not isinstance(rhs, Function):
 			rhs = Function(rhs)
-		left_adouble = Arg(self.eval(),self)
-		right_adouble = Arg(rhs.eval(), rhs)
-		return Function([left_adouble, right_adouble], function_type='mul')
+		return Function([self, rhs], function_type='mul')	
 
 	def __radd__(self,lhs):
 		return self + lhs
 	
-	def __rmul__(self,lhs):
-		return self * lhs
+	def __str__(self):
+		return '%s%s:(%s,%s)'%(self.type,str(self.id),str(self.x), str(self.xbar))
 	
+	def __repr__(self):
+		return self.__str__()
+
 	def eval(self):
 		if self.type == 'var':
-			return self.args[0].x
-		elif self.type == 'id':
-			return self.args[0].x
+			return Tc(self.args)
+		
 		elif self.type == 'add':
-			return self.args[0].x + self.args[1].x
+			return self.args[0].x + self.args[1].x #[self.args[0].x[0] + self.args[1].x[0], self.args[0].x[1] + self.args[1].x[1]]
+
 		elif self.type == 'mul':
 			return self.args[0].x * self.args[1].x
 
 	def reval(self):
 		if self.type == 'var':
 			pass
-		elif self.type == 'id':
-			self.args[0].source.xbar += self.xbar
+		
 		elif self.type == 'add':
-			self.args[0].source.xbar += self.xbar
-			self.args[1].source.xbar += self.xbar
+			self.args[0].xbar = self.xbar
+			self.args[1].xbar = self.xbar
+
 		elif self.type == 'mul':
-			self.args[0].source.xbar += self.xbar * self.args[1].x
-			self.args[1].source.xbar += self.xbar * self.args[0].x
+			self.args[0].xbar = self.xbar * self.args[1].x
+			self.args[1].xbar = self.xbar * self.args[0].x
 
-	def __str__(self):
-		return '%s%s:(%f,%f)'%(self.type,str(self.id),self.eval(), self.xbar)
-	def __repr__(self):
-		return self.__str__()
-
-class Arg:
-	def __init__(self,x, source = None):
-		self.x = x
-		self.source = source
-
-	def __str__(self):
-		return '(x=%s,source=%s)'%(str(self.x), str(self.source))
-	def __repr__(self):
-		return self.__str__()
 
 class CGraph:
 	"""
@@ -90,26 +85,24 @@ class CGraph:
 	The Graph of y = x1(x2+x3) looks like
 
 	--- independent variables
-	v1: a1(x1,None)
-	v2: a2(x2,None)
-	v3: a3(x3,None)
+	v1(x1): None
+	v2(x2): None
+	v3(x3): None
 
 	--- function operations
-	+4: [a4(v2.eval, v2), a5(v3.eval, v3)]
-	*5: [a6(+4.eval,+4), a7(v1.eval,v1)]
+	+4(v2.x + v3.x): [v2,v3] 
+	*5(v1.x * +4.x): [v1,+4]
 
 	--- dependent variables
-	v6: a8(*5.eval,*5)
+	v6(*5.x): [*5]
 	
 	"""
 	
 	def __init__(self):
-		self.argCount = 0
 		self.functionCount = 0
 		self.functionList = []
 		self.dependentFunctionList = []
 		self.independentFunctionList = []
-		Arg.cgraph = self
 		Function.cgraph = self
 
 	def __str__(self):
@@ -118,14 +111,11 @@ class CGraph:
 	def forward(self,x):
 		# populate independent arguments with new values
 		for nf,f in enumerate(self.independentFunctionList):
-			f.args[0].x = x[nf]
+			f.args = x[nf]
 			
 		# traverse the computational tree
 		for f in self.functionList:
-			if f.type == 'var':
-				continue
-			for a in f.args:
-				a.x = a.source.eval()
+			f.x = f.eval()
 
 	def reverse(self,xbar):
 		for nf,f in enumerate(self.dependentFunctionList):
@@ -134,98 +124,63 @@ class CGraph:
 		for f in self.functionList[::-1]:
 			f.reval()
 
-	def plot(self):
-		import pygraphviz
-		import os
-		
-		A = pygraphviz.agraph.AGraph(directed=True)
-		A.node_attr['fillcolor']="#000000"
-		A.node_attr['shape']='rect'
-		A.node_attr['width']='0.3'
-		A.node_attr['height']='0.3'
-		A.node_attr['fontcolor']='#ffffff'
-		A.node_attr['style']='filled'
-		A.node_attr['fixedsize']='true'
+#def tape(f,in_x):
+	#x = in_x[:]
+	#N = len(x)
+	#cg = CGraph()
+	#ax = numpy.array([Function(x[n]) for n in range(N)])
+	#cg.independentFunctionList = ax
+	#ay = f(ax)
+	#cg.dependentFunctionList = numpy.array([ay])
+	#return cg
 
-		# build graph
-		for f in cg.functionList:
-			if f.type == 'var':
-				continue
-			for a in f.args:
-				A.add_edge(a.source.id, f.id, label='%s'%a.x)
-				#e = A.get_edge(a.source.id, f.id)
-				#e.attr['color']='green'
-				#e.attr['label']='a'
+#def gradient_from_graph(cg,x=None):
+	#if x != None:
+		#cg.forward(x)
+	#cg.reverse(numpy.array([1.]))
+	#N = len(cg.independentFunctionList)
+	#return numpy.array([cg.independentFunctionList[n].xbar for n in range(N)])
 
-		for nf,f in enumerate(cg.functionList):
-			s = A.get_node(nf)
-			vtype = f.type
-
-			if vtype == 'add':
-				s.attr['label']='+%d'%nf
-			elif vtype == 'mul':
-				s.attr['label']='*%d'%nf
-			elif vtype == 'var':
-				s.attr['fillcolor']="#FFFF00"
-				s.attr['shape']='circle'
-				s.attr['label']= 'v_%d'%nf
-				s.attr['fontcolor']='#000000'
-
-		print A.string() # print to screen
-
-		A.write("trash/computational_graph.dot")
-		os.system("dot  trash/computational_graph.dot -Tsvg -o trash/computational_graph.svg")
-		os.system("dot  trash/computational_graph.dot -Tpng -o trash/computational_graph.png")
-		
-
-
-
-def tape(f,in_x):
-	x = in_x[:]
-	N = len(x)
-	cg = CGraph()
-	ax = numpy.array([Function(x[n]) for n in range(N)])
-	cg.independentFunctionList = ax
-	ay = f(ax)
-	cg.dependentFunctionList = numpy.array([ay])
-	return cg
-
-def gradient_from_graph(cg,x=None):
-	if x != None:
-		cg.forward(x)
-	cg.reverse(numpy.array([1.]))
-	N = len(cg.independentFunctionList)
-	return numpy.array([cg.independentFunctionList[n].xbar for n in range(N)])
-
-def gradient(f, in_x):
-	cg = tape(f,in_x)
-	return gradient_from_graph(cg)
+#def gradient(f, in_x):
+	#cg = tape(f,in_x)
+	#return gradient_from_graph(cg)
 
 
 if __name__ == "__main__":
-	import numpy
-	import numpy.linalg
-	N = 10
-	A = 0.125 * numpy.eye(N)
-	x = numpy.ones(N)
+	cg = CGraph()
+	x = Function([11.,0])
+	y = Function([13.,0.])
+	z = x*y
+	cg.independentFunctionList = [x,y]
+	cg.dependentFunctionList = [z]
+	cg.forward([[11.,0.],[13.,1.]])
+	print z
+	cg.reverse([Tc([1.,0.])])
+	print x.xbar
+	print y.xbar
+	#import numpy
+	#import numpy.linalg
+	#N = 10
+	#A = 0.125 * numpy.eye(N)
+	#x = numpy.ones(N)
 
-	def f(x):
-		return 0.5*numpy.dot(x, numpy.dot(A,x))
+	#def f(x):
+		#return 0.5*numpy.dot(x, numpy.dot(A,x))
 
 
-	# normal function evaluation
-	y = f(x)
+	## normal function evaluation
+	#y = f(x)
 
-	# taping + reverse evaluation
-	g_reverse = gradient(f,x)
+	## taping + reverse evaluation
+	#g_reverse = gradient(f,x)
 
-	print g_reverse
+	#print g_reverse
 
-	# taping
-	cg = tape(f,x)
-	print gradient_from_graph(cg)
+	## taping
+	#cg = tape(f,x)
+	#print gradient_from_graph(cg)
 
-	cg.plot()
+	#cg.plot()
 
 
 
