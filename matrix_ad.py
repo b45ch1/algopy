f54f1e1d9820c5fc9501bc3296678e6c5a4de3f5
Sebastian Walter <walter@mathematik.hu-berlin.de>
2008-10-29 17:55:58 +0100

    working version of matrix forward and reverse mode implementation is not very elegant and needs a face lift
    added unit tests for matrix_ad

--- matrix_ad.py
+++ matrix_ad.py
@@ -6,13 +6,81 @@ from numpy.linalg import *
 import numpy
 import numpy.linalg
 
+
+
+class Tc:
+	"""
+	Taylor coefficients Tc.
+	One can propagate more than one direction at a time (called vector mode) by initializing with
+	mytc = Tc(x0, [[x11,...,x1Ndir],
+	               [x21,...,x2Ndir],
+		            ....
+		           [xD1,...,xDNdir]])
+		       
+	For convenience it is also possible to initialize with
+	mytc = Tc([x0,x1,...,xD])
+	i.e. only propagating one direction at a time.
+	"""
+	def __init__(self,in_t0,tc=None):
+		if tc == None:
+			self.t0 = in_t0[0]
+			self.tc = asarray(in_t0[1:])
+		else:	
+			self.t0 = in_t0
+			self.tc = asarray(tc)
+	
+	def copy(self):
+		return Tc(self.t0, self.tc.copy())
+	
+	def set_zero(self):
+		self.t0 = 0.
+		self.tc[:] = 0
+		return self
+
+	def __add__(self,rhs):
+		return Tc(self.t0 + rhs.t0, self.tc[:] + rhs.tc[:])
+	
+	def __mul__(self,rhs):
+		#print 'shape(self.tc)',shape(self.tc)
+		#print 'shape(rhs.tc)',shape(rhs.tc)
+		#print 'self.tc=',self.tc
+		#print 'rhs.tc=',rhs.tc
+		#d=2
+		#print 'self.tc[:d]',self.tc[:d-1]
+		#print 'rhs.tc[d-2::-1]',rhs.tc[d-2::-1]
+		#print 'self.tc[:d-1] * rhs.tc[d-2::-1]', self.t0*rhs.tc[d-1] +  self.tc[:d-1] * rhs.tc[d-2::-1] + self.tc[d-1] * rhs.t0
+		#exit()
+		
+		def middle_taylor_series(d):
+			if d>1:
+				return 	npy.sum(self.tc[:d-1] * rhs.tc[d-2::-1], axis = 0)
+			return 0
+		
+		D = shape(self.tc)[0]
+		E = shape(rhs.tc)[0]
+		assert D==E
+		return Tc(self.t0 * rhs.t0,
+					 npy.array(  [ self.t0*rhs.tc[d-1] + middle_taylor_series(d)  + self.tc[d-1] * rhs.t0 for d in range(1,D+1)]
+					))
+
+	def __str__(self):
+		return 'Tc(%s,%s)'%(str(self.t0),str(self.tc))
+
+
+
 class Mtc:
+	"""
+	Matrix Taylor Coefficients
+	This class implements Taylor arithmetic on matrices, i.e.
+	[A] = \sum_{d=0} A_d t^d
+	A_d = \frac{d^d}{dt^d}|_{t=0} \sum_{d=0} A_d t^d
+	"""
 	def __init__(self, X, Xdot = None):
 		if Xdot == None:
 			self.Xdot = zeros(shape(X))
 		else:
-			self. Xdot = Xdot
-		self.X = X
+			self.Xdot = asarray(Xdot)
+		self.X = asarray(X)
 
 	def __add__(self,rhs):
 		return Mtc(self.X + rhs.X, self.Xdot + rhs.Xdot)
@@ -26,6 +94,12 @@ class Mtc:
 	def inv(self):
 		Y0 = numpy.linalg.inv(self.X)
 		return Mtc( Y0, dot(Y0, dot(self.Xdot, Y0) ))
+
+	def trace(self):
+		return Mtc( [[self.X.trace()]], [[self.Xdot.trace()]])
+
+	def __getitem__(self, key):
+		return Mtc(self.X[key], self.Xdot[key])
 
 	def copy(self):
 		return Mtc(self.X.copy(), self.Xdot.copy())
@@ -44,13 +118,29 @@ class Mtc:
 	def __repr__(self):
 		return self.__str__()
 
+
+# Numpy wrapper
+#--------------
 def inv(X):
 	if X.__class__ == Mtc:
 		return X.inv()
 	else:
 		return numpy.linalg.inv(X)
+#--------------
 
+
+# convenience functions (should be as few as possible)
+#-----------------------------------------------------
 def convert(in_X):
+	"""
+	expects a matrix/list consisting of entries of type Mtc, e.g.
+	in_X = [[Mtc1,Mtc2],[Mtc3,Mtc4]]
+	and returns
+	Mtc([[Mtc1.X,Mtc2.X],[Mtc3.X,Mtc4.X]], [[Mtc1.Xdot,Mtc2.Xdot],[Mtc3.Xdot,Mtc4.Xdot]])
+
+	if a matrix/list of Function is provided, the Taylor series are extracted
+	"""
+	
 	in_X = asarray(in_X).copy()
 
 	# find total size
@@ -67,21 +157,43 @@ def convert(in_X):
 
 	X = zeros((rowsums[-1],colsums[-1]))
 	Xdot = X.copy()
-	
-	for r in range(Rb):
-		for c in range(Cb):
-			X[rowsums[r]:rowsums[r+1], colsums[c]:colsums[c+1]] = in_X[r,c].X
-			Xdot[rowsums[r]:rowsums[r+1], colsums[c]:colsums[c+1]] = in_X[r,c].Xdot
+
+	# check if input is list of Functions
+	if in_X[0,0].__class__ == Function:
+		for r in range(Rb):
+			for c in range(Cb):
+				X[rowsums[r]:rowsums[r+1], colsums[c]:colsums[c+1]] = in_X[r,c].x.X
+				Xdot[rowsums[r]:rowsums[r+1], colsums[c]:colsums[c+1]] = in_X[r,c].x.Xdot
+
+	else:
+		for r in range(Rb):
+			for c in range(Cb):
+				X[rowsums[r]:rowsums[r+1], colsums[c]:colsums[c+1]] = in_X[r,c].X
+				Xdot[rowsums[r]:rowsums[r+1], colsums[c]:colsums[c+1]] = in_X[r,c].Xdot
 
 	return Mtc(X,Xdot)
-	
 
+
+def convert_from_tc_to_mtc(in_X):
+	R,C = shape(in_X)
+	X = zeros((R,C))
+	Xdot = X.copy()
+
+	for r in range(R):
+		for c in range(C):
+			X[r,c] = in_X[r,c].t0
+			Xdot[r,c] = in_X[r,c].tc[0]
+	return Mtc(X,Xdot)
+#-----------------------------------------------------
 
 
 class Function:
 	def __init__(self, args, function_type='var'):
 		if function_type == 'var':
-			self.type = 'var'
+			if type(args) == list:
+				self.type = 'com'
+			else:
+				self.type = 'var'
 		elif function_type == 'id':
 			self.type = 'id'
 		elif function_type == 'com':
@@ -92,25 +204,54 @@ class Function:
 			self.type = 'mul'
 		elif function_type == 'dot':
 			self.type = 'dot'
+		elif function_type == 'trace':
+			self.type = 'trace'
 		else:
 			raise NotImplementedError('function_type must be either \'v\' or \'mul\' or  \'add\'')
 
 		self.args = args
 		self.x = self.eval()
-		self.xbar = self.x.copy().set_zero()
+		self.xbar_from_x()
 		self.id = self.cgraph.functionCount
 		self.cgraph.functionCount += 1
 		self.cgraph.functionList.append(self)
 
 
+	# convenience functions
+	# ---------------------
 	def as_function(self, in_x):
 		if not isinstance(in_x, Function):
 			fun = Function(self.x.copy().set_zero())
 			fun.x.t0 = in_x
 			return fun
 		return in_x
-		
-		
+
+	def xbar_from_x(self):
+		if type(self.x) == list:
+			self.xbar = []
+			for r in self.x:
+				self.xbar.append([c.x.copy().set_zero() for c in r])
+			return
+		else:
+			self.xbar = self.x.copy().set_zero()
+			
+
+
+
+	def __str__(self):
+		try:
+			ret = '%s%s:\n(x=\n%s)\n(xbar=\n%s)'%(self.type,str(self.id),str(self.x),str(self.xbar))
+		except:
+			ret = '%s%s:(x=%s)'%(self.type,str(self.id),str(self.x))
+		return ret
+
+	def __repr__(self):
+		return self.__str__()
+	# ---------------------
+
+
+	# overloaded matrix operations
+	# ----------------------------
 	def __add__(self,rhs):
 		rhs = self.as_function(rhs)
 		return Function([self, rhs], function_type='add')
@@ -124,20 +265,26 @@ class Function:
 
 	def __rmul__(self,lhs):
 		return self * lhs
-	
-	def __str__(self):
-		try:
-			ret = '%s%s:\n(x=\n%s)\n(xbar=\n%s)'%(self.type,str(self.id),str(self.x),str(self.xbar))
-		except:
-			ret = '%s%s:(x=%s)'%(self.type,str(self.id),str(self.x))
-		return ret
-	
-	def __repr__(self):
-		return self.__str__()
 
+	def dot(self,rhs):
+		rhs = self.as_function(rhs)
+		return Function([self, rhs], function_type='dot')
+
+	def trace(self):
+		return Function([self], function_type='trace')
+
+	def shape(self):
+		return numpy.shape(self.x.X)
+	# ----------------------------		
+
+	# forward and reverse evaluation
+	# ------------------------------
 	def eval(self):
 		if self.type == 'var':
 			return self.args
+
+		elif self.type == 'com':
+			return convert(self.args)
 		
 		elif self.type == 'add':
 			return self.args[0].x + self.args[1].x
@@ -146,7 +293,13 @@ class Function:
 			return self.args[0].x * self.args[1].x
 
 		elif self.type == 'dot':
-			return numpy.dot(self.args[0].x,self.args[1].x)
+			return self.args[0].x.dot(self.args[1].x)
+
+		elif self.type == 'trace':
+			return self.args[0].x.trace()
+
+		else:
+			raise Exception('Unknown function "%s". Please add rule to Mtc.eval()'%self.type)
 
 	def reval(self):
 		if self.type == 'var':
@@ -161,8 +314,48 @@ class Function:
 			self.args[1].xbar += self.xbar * self.args[0].x
 
 		elif self.type == 'dot':
-			self.args[0].xbar +=  numpy.dot(self.args[1].x, self.xbar)
-			self.args[1].xbar +=  numpy.dot(self.xbar, self.args[0].x)
+			self.args[0].xbar +=  self.args[1].x.dot(self.xbar)
+			self.args[1].xbar +=  self.xbar.dot(self.args[0].x)
+
+		elif self.type == 'trace':
+			N = self.args[0].x.shape()[0]
+			self.args[0].xbar += Mtc( self.xbar.X[0,0]*numpy.eye(N),  self.xbar.Xdot[0,0]*numpy.eye(N))
+			
+
+		elif self.type == 'com':
+			Rb,Cb = shape(self.args)
+			#print 'xbar.shape()=',self.xbar.shape()
+			args = asarray(self.args)
+			rows = []
+			cols = []
+			#print type(args)
+			for r in range(Rb):
+				rows.append(args[r,0].shape()[0])
+			for c in range(Cb):
+				cols.append(args[0,c].shape()[0])
+
+			#print rows
+			#print cols
+
+			rowsums = [ int(sum(rows[:r])) for r in range(0,Rb+1)]
+			colsums = [ int(sum(cols[:c])) for c in range(0,Cb+1)]
+
+			#print rowsums
+			#print colsums
+			#print 'shape of xbar=', shape(self.xbar.X)
+			#print 'shape of x=', shape(self.x.X)
+			
+			for r in range(Rb):
+				for c in range(Cb):
+					#print 'args[r,c].xbar=\n',args[r,c].xbar.shape()
+					#print 'rhs=\n', self.xbar[rowsums[r]:rowsums[r+1],colsums[c]:colsums[c+1]].shape()
+					
+					args[r,c].xbar += self.xbar[rowsums[r]:rowsums[r+1],colsums[c]:colsums[c+1]]
+		
+		else:
+			raise Exception('Unknown function "%s". Please add rule to Mtc.reval()'%self.type)
+			
+	# ------------------------------
 
 class CGraph:
 	"""
@@ -203,13 +396,66 @@ class CGraph:
 			f.x = f.eval()
 
 	def reverse(self,xbar):
+		if numpy.size(self.dependentFunctionList) == 0:
+			print 'You forgot to specify which variables are dependent!\n e.g. with cg.dependentFunctionList = [F1,F2]'
+			return 
+		
 		for nf,f in enumerate(self.dependentFunctionList):
 			f.xbar = xbar[nf]
 
 		for f in self.functionList[::-1]:
 			f.reval()
 
+	def plot(self):
+		import pygraphviz
+		import os
+		
+		A = pygraphviz.agraph.AGraph(directed=True)
+		A.node_attr['fillcolor']="#000000"
+		A.node_attr['shape']='rect'
+		A.node_attr['width']='0.5'
+		A.node_attr['height']='0.5'
+		A.node_attr['fontcolor']='#ffffff'
+		A.node_attr['style']='filled'
+		A.node_attr['fixedsize']='true'
 
+		# build graph
+		for f in self.functionList:
+			if f.type == 'var':
+				A.add_node(f.id)
+				continue
+			for a in numpy.ravel(f.args):
+				A.add_edge(a.id, f.id)
+				#e = A.get_edge(a.source.id, f.id)
+				#e.attr['color']='green'
+				#e.attr['label']='a'
+
+		for nf,f in enumerate(self.functionList):
+			s = A.get_node(nf)
+			vtype = f.type
+
+			if vtype == 'add':
+				s.attr['label']='+%d'%nf
+			elif vtype == 'mul':
+				s.attr['label']='*%d'%nf
+			elif vtype == 'var':
+				s.attr['fillcolor']="#FFFF00"
+				s.attr['shape']='circle'
+				s.attr['label']= 'v_%d'%nf
+				s.attr['fontcolor']='#000000'
+			elif vtype == 'dot':
+				s.attr['label']='dot%d'%nf
+			elif vtype == 'com':
+				s.attr['label']='com%d'%nf
+				
+			elif vtype == 'trace':
+				s.attr['label']='trace%d'%nf
+				
+		print A.string() # print to screen
+
+		A.write("trash/computational_graph.dot")
+		os.system("dot  trash/computational_graph.dot -Tsvg -o trash/computational_graph.svg")
+		os.system("dot  trash/computational_graph.dot -Tpng -o trash/computational_graph.png")
 
 
 if __name__ == "__main__":
@@ -223,7 +469,8 @@ if __name__ == "__main__":
 
 
 	AW = [[AX,AY],[AY,AX]]
-	print convert(AW)
+	#print AW
+	#print convert(AW)
 
 	#print AX + AY
 	#print AX * AY
@@ -232,9 +479,18 @@ if __name__ == "__main__":
 	#print inv(X)
 
 	#### Testing Taping
-	#cg = CGraph()
-	#FX = Function(AX)
-	#FY = Function(AY)
+	cg = CGraph()
+	FX = Function(AX)
+	FY = Function(AY)
+	Fzer = Function(Mtc(zeros((2,2))))
+	FU = FX.dot(FY)
+
+	#FW = Function(AW)
+	FV = Function([[FX,FY],[Fzer,FX]])
+	FU = Function([[FX,FY],[Fzer,FX]])
+	FW = FV*FU
+	#print FW
+	#print FV
 
 	#FZ = dot(FX,FY)
 	#FZbar = Mtc(eye(2))
@@ -242,5 +498,6 @@ if __name__ == "__main__":
 	#cg.dependentFunctionList=[FZ]
 	#cg.reverse([FZbar])
 
-	#print cg
+	#print cg.functionList
+	cg.plot()
 
---
+++ unit_tests/unit_test_matrix_ad.py
@@ -0,0 +1,236 @@
+#!/usr/bin/env python
+import sys
+import numpy
+import numpy.linalg
+from numpy import *
+
+sys.path = ['..'] + sys.path
+from matrix_ad import *
+
+def test_trace_forward():
+	x = Mtc(ones((2,2)),ones((2,2)))
+	y = x.trace()
+	assert trace(x.X) == y.X[0,0] and trace(x.Xdot) == y.Xdot[0,0]
+
+def test_1x1dot1x1_reverse():
+	"""
+	f = x.dot(y)  x and are (1,1)-arrays
+	x = 1
+	y = 2
+
+	therefore 
+	grad f = [2,1].T
+	hess f = [[0,1],[1,0]]
+	"""
+	cg = CGraph()
+	Fq = [Function(Mtc([[1.]],[[1.]])), Function(Mtc([[2.]],[[0.]]))]
+	#zer = Mtc(array([[0.]]))
+	#Fzer = Function(zer)
+
+	FR = Fq[0].dot(Fq[1])
+
+	cg.independentFunctionList = Fq
+	cg.dependentFunctionList = [FR]
+	cg.reverse([Mtc([[1.]])])
+
+	# check gradient
+	assert Fq[0].xbar.X[0,0] == 2
+	assert Fq[1].xbar.X[0,0] == 1
+
+	#check first column of the hessian
+	assert Fq[0].xbar.Xdot[0,0] == 0
+	assert Fq[1].xbar.Xdot[0,0] == 1
+
+def test_2x2dot2x2_reverse():
+	"""
+	C = A.dot(B)  A,B are (2,2)-arrays
+	"""
+	A =    array([[3.,1.],[2.,4.]])
+	Adot = array([[1.,0.],[0.,0.]])
+	B =    array([[5.,2.],[1.,7.]])
+	Bdot = array([[0.,0.],[0.,0.]])
+	C = numpy.dot(A,B)
+	Cdot = numpy.dot(A,Bdot) + numpy.dot(Adot,B)
+	
+	Cbar = array([[1.,0.],[0.,0.]])
+	
+	cg = CGraph()
+	FA = Function(Mtc(A,Adot))
+	FB = Function(Mtc(B,Bdot))
+
+	FC = FA.dot(FB)
+
+	cg.independentFunctionList = [FA,FB]
+	cg.dependentFunctionList = [FC]
+	cg.reverse([Mtc(Cbar)])
+
+	# forward evaluation
+	assert numpy.prod(C == FC.x.X)
+	assert numpy.prod(Cdot == FC.x.Xdot)
+
+	# reverse evaluation
+	assert numpy.prod(FA.xbar.X == dot(B,Cbar))
+	assert numpy.prod(FB.xbar.X == dot(Cbar,A))
+	assert False # FA.xbar.Xdot has to be verified
+	assert False # FB.xbar.Xdot has to be verified
+
+def test_matrix_assembly_2x2():
+	"""
+	C = [[A,B],[B,A]]
+	A,B are (2,2)-arrays
+	"""
+	A =    array([[1.,1.],[1.,1.]])
+	Adot = array([[2.,2.],[2.,2.]])
+	B =    array([[3.,3.],[3.,3.]])
+	Bdot = array([[4.,4.],[4.,4.]])
+
+	C = zeros((4,4))
+	C[:2,:2] = A
+	C[:2,2:] = B
+	C[2:,:2] = B
+	C[2:,2:] = A
+
+	Cdot = zeros((4,4))
+	Cdot[:2,:2] = Adot
+	Cdot[:2,2:] = Bdot
+	Cdot[2:,:2] = Bdot
+	Cdot[2:,2:] = Adot
+
+	Cbar = 5*ones((4,4))
+	Cbardot = 6*ones((4,4))
+
+	cg = CGraph()
+	FA = Function(Mtc(A,Adot))
+	FB = Function(Mtc(B,Bdot))
+
+	FC = Function([[FA,FB],[FB, FA]])
+
+	cg.independentFunctionList = [FA,FB]
+	cg.dependentFunctionList = [FC]
+	cg.reverse([Mtc(Cbar,Cbardot)])
+
+	# forward evaluation
+	assert numpy.prod(FC.x.X == C)
+	assert numpy.prod(FC.x.Xdot == Cdot)
+
+	# reverse evaluation
+	assert numpy.prod(FA.xbar.X == 2*FC.xbar[:2,:2].X)
+	assert numpy.prod(FB.xbar.X == 2*FC.xbar[2:,2:].X)
+	
+	assert numpy.prod(FA.xbar.Xdot == 2*FC.xbar[:2,:2].Xdot)
+	assert numpy.prod(FB.xbar.Xdot == 2*FC.xbar[2:,2:].Xdot)
+
+def test_trace_2x2():
+	"""
+	"""
+	A =    array([[1.,1.],[1.,1.]])
+	Adot = array([[2.,2.],[2.,2.]])
+	trbar = array([[13.]])
+	trbardot = array([[0.]])
+	
+	cg = CGraph()
+	FA = Function(Mtc(A,Adot))
+	Ftr = FA.trace()
+	cg.independentFunctionList = [FA]
+	cg.dependentFunctionList = [Ftr]
+	cg.reverse([Mtc(trbar,trbardot)])
+
+	print FA.xbar.X
+	
+	assert False
+	
+
+
+def test_newtons_method():
+	"""
+	min_q Phi(C(q))
+
+	Phi(C) = trace(C)
+	C = [[q1**2, 0], [0 ,q2**2 ]]
+
+	staring point: (13.,17.)
+	solution: (0.,0.)
+
+	Newton's method with full steps should converge in one step
+	"""
+
+	H = zeros((2,2))
+	g = zeros((2,2)) # first column, gradient of first direct. derv., second second direct. derv, should be the same
+
+	for n in range(2):
+		# directional derivatives to evaluate columns of the hessian
+		q1 = array([[13.]])
+		q2 = array([[17.]])
+		ze = array([[0.]])
+
+		q1dot = array([[1.*(n==0)]])
+		q2dot = array([[1.*(n==1)]])
+
+		zedot = array([[0.]])
+
+		cg = CGraph()
+		Fq1 = Function(Mtc(q1, q1dot))
+		Fq2 = Function(Mtc(q2, q2dot))
+		Fze = Function(Mtc(ze, zedot))
+		FC = Function([[Fq1*Fq1,Fze],[Fze,Fq2*Fq2]])
+		FPhi = FC.trace()
+
+		Phibar = array([[1.]])
+		Phibardot = array([[0.]])
+		
+		cg.independentFunctionList = [Fq1,Fq2]
+		cg.dependentFunctionList = [FPhi]
+		cg.reverse([Mtc(Phibar, Phibardot)])
+		
+		
+		g[0,n] = Fq1.xbar.X[0,0]
+		g[1,n] = Fq2.xbar.X[0,0]
+		H[0,n] = Fq1.xbar.Xdot[0,0]
+		H[1,n] = Fq2.xbar.Xdot[0,0]
+
+
+
+	# compute new search direction
+	delta_q = numpy.linalg.solve(H,-g[:,0])
+	q_plus = [13.,17.] + delta_q
+	assert numpy.prod(q_plus == [0.,0.])
+
+	
+
+
+	
+
+if __name__ == "__main__":
+	#q = [Mtc([[1.]],[[1.]]), Mtc([[2.]],[[0.]])]
+	
+	#C = [[q[0],zer],[zer,q[1]]]
+
+	cg = CGraph()
+	Fq = [Function(Mtc([[3.,1.],[2.,4.]],[[1.,0.],[0.,0.]])),
+	      Function(Mtc([[5.,2.],[1.,7.]],[[0.,0.],[0.,0.]]))]
+
+	FR = Fq[0].dot(Fq[1])
+	FT = FR.trace()
+
+	cg.independentFunctionList = Fq
+	cg.dependentFunctionList = [FT]
+	cg.reverse([Mtc([[1.]])])
+
+	print cg
+
+	#zer = Mtc(array([[0.]]))
+	#Fzer = Function(zer)
+	#FC = Function([[Fq[0],Fzer],[Fzer, Fq[1]]])
+
+	#print 'FC=',FC
+	#FTR = FC.trace()
+
+
+	#x = array([[Tc([1.,0.]), Tc([2.,0.])]])
+	#y = convert_from_tc_to_mtc(x)
+
+	#print 'x=',x
+	#print 'y=',y
+	
+	
+
