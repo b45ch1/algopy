1e5ba70dc12a83fbaf962cff8542dd05908aa74a
Sebastian Walter <sebastian.walter@gmail.com>
2009-12-30 13:43:00 +0100

    adapted ICCS2010 to odoe notation

--- documentation/ICCS2010/numerical_example_odoe.py
+++ documentation/ICCS2010/numerical_example_odoe.py
@@ -1,14 +1,19 @@
 from algopy.utp.utpm import *
 
-D,Nx,Ny,NF = 2,3,3,20
-P = Ny
-x = UTPM(numpy.random.rand(D,P,Nx))
-y = UTPM(numpy.random.rand(D,P,Ny))
+# Np = number of parameters p
+# Nq = number of control variables q
+# Nm = number of measurements
+# P  = number of vectorized operations at once (SPMD)
 
-F = UTPM(numpy.zeros((D,P,NF)))
+D,Nq,Np,Nm = 2,3,3,20
+P = Np
+q = UTPM(numpy.random.rand(D,P,Nq))
+p = UTPM(numpy.random.rand(D,P,Np))
 
-for nf in range(NF):
-    F[nf] =  numpy.sum([ (nf+1.)**n * x[n]*y[-n] for n in range(Nx)])
+F = UTPM(numpy.zeros((D,P,Nm)))
+
+for nm in range(Nm):
+    F[nm] =  numpy.sum([ (nm+1.)**n * q[n]*p[-n] for n in range(Nq)])
 
 J = F.FtoJT().T
 Q,R = UTPM.qr(J)
@@ -18,6 +23,7 @@ D = UTPM.solve(R.T,Id)
 C = UTPM.solve(D,R)
 l,U = UTPM.eig(C)
 
+print l
 l11 = l.max()
 
 print l11
