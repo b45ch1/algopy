5b197dff3a9f4b7f26eb61392551a1d4879e1f52
Sebastian Walter <sebastian.walter@gmail.com>
2009-12-31 15:01:14 +0100

    added eigenvalue decomposition but having problem with segfaults when matrix A is getting larger (probably a numpy bug...)

--- documentation/ICCS2010/numerical_accuracy_check.py
+++ documentation/ICCS2010/numerical_accuracy_check.py
@@ -7,15 +7,19 @@ import algopy.utp.utpm as utpm
 # M is the number of rows of the matrix A
 # N is the number of columsn of the matrix A
 
+
+
+# --------------------------------------------
+# compute push forward of the QR decomposition
+# --------------------------------------------
 D,P,M,N = 5,5,100,5
+print 'testing SPMD (%d datasets) differentiated QR decomposition for matrix A.shape = (%d,%d) up to %d\'th order'%(P,M,N,D)
 
 A = utpm.UTPM(numpy.random.rand(D,P,M,N))
 
-# compute push forward of the QR decomposition
 tic = time.time()
 Q,R = utpm.UTPM.qr(A)
 toc = time.time()
-
 runtime_push_forward = toc - tic
 
 # check that normal QR decomposition matches the push forward version
@@ -25,6 +29,7 @@ for p in range(P):
     numpy.testing.assert_array_almost_equal(Q.data[0,p],Q0)
     numpy.testing.assert_array_almost_equal(R.data[0,p],R0)
 
+# check that the pushforward computed correctly
 B = utpm.UTPM.dot(Q,R)
 print 'largest error for all QR decompositions and derivative degrees is: ',numpy.max(A.data - B.data)
 
@@ -37,4 +42,43 @@ for p in range(P):
 toc = time.time()
 runtime_normal = toc - tic
 
-print 'measured runtime ratio push_forward/normal in sec: ', runtime_push_forward/runtime_normal
+print 'measured runtime ratio push_forward/normal: ', runtime_push_forward/runtime_normal
+
+
+# ----------------------------------------------------
+# compute push forward of the eigenvalue decomposition
+# ----------------------------------------------------
+D,P,N = 5,5,8
+print 'testing SPMD (%d datasets) differentiated eig decomposition for matrix A.shape = (%d,%d) up to %d\'th order'%(P,N,N,D)
+
+
+# create symmetric matrix
+A = utpm.UTPM(numpy.random.rand(D,P,N,N))
+A = utpm.UTPM.dot(A.T,A)
+
+tic = time.time()
+l,Q = utpm.UTPM.eig(A)
+toc = time.time()
+runtime_push_forward = toc - tic
+
+# check that normal QR decomposition matches the push forward version
+for p in range(P):
+    A0 = numpy.ascontiguousarray(A.data[0,p])
+    l0,Q0 = numpy.linalg.eig(A0)
+    numpy.testing.assert_array_almost_equal(Q.data[0,p],Q0)
+    numpy.testing.assert_array_almost_equal(l.data[0,p],l0)
+
+# check that the pushforward computed correctly
+L = utpm.UTPM.diag(l)
+B = utpm.UTPM.dot(utpm.UTPM.dot(Q,L), Q.T)
+print 'largest error for all eigenvalue decompositions and derivative degrees is: ',numpy.max(A.data - B.data)
+
+# check runtime
+tic = time.time()
+for p in range(P):
+    A0 = numpy.ascontiguousarray(A.data[0,p])
+    Q0,R0 = numpy.linalg.eig(A0)
+toc = time.time()
+runtime_normal = toc - tic
+
+print 'measured runtime ratio push_forward/normal: ', runtime_push_forward/runtime_normal
