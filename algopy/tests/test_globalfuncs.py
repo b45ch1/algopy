4948e0d01b32181fe4390801041e617fc0f3c790
Sebastian F. Walter <sebastian.walter@gmail.com>
2010-01-27 18:26:49 +0100

    removed deprecated test of globalfuncs in UTPM

--- algopy/tests/test_globalfuncs.py
+++ algopy/tests/test_globalfuncs.py
@@ -37,6 +37,37 @@ class Test_Global_Functions_on_UTPM_instances(TestCase):
         x = UTPM(numpy.random.rand(D,P,N,N))
         assert_array_almost_equal(inv(x).data, UTPM.inv(x).data)
 
+class Test_global_functions(TestCase):
+  
+ 
+    def test_numpy_overrides(self):
+
+        X = 2 * numpy.random.rand(2,2,2,2)
+        Y = 3 * numpy.random.rand(2,2,2,2)
+
+        AX = UTPM(X)
+        AY = UTPM(Y)
+
+        assert_array_almost_equal( UTPM.dot(AX,AY).data, dot(AX,AY).data)
+        assert_array_almost_equal( UTPM.inv(AX).data,  inv(AX).data)
+        
+        assert_array_almost_equal( UTPM.trace(AX).data,  trace(AX).data)
+
+    
+    # def test_convert(self):
+        # X1 = 2 * numpy.random.rand(2,2,2,2)
+        # X2 = 2 * numpy.random.rand(2,2,2,2)
+        # X3 = 2 * numpy.random.rand(2,2,2,2)
+        # X4 = 2 * numpy.random.rand(2,2,2,2)
+        # AX1 = UTPM(X1)
+        # AX2 = UTPM(X2)
+        # AX3 = UTPM(X3)
+        # AX4 = UTPM(X4)
+        # AY = combine_blocks([[AX1,AX2],[AX3,AX4]])
+
+        # assert_array_equal(numpy.shape(AY.data),(2,2,4,4))
+
+
 if __name__ == "__main__":
     run_module_suite()
 
--- run_tests.py
+++ run_tests.py
@@ -1,6 +1,8 @@
-from algopy.tests.test_tracer import *
+from algopy.tracer.tests.test_tracer import *
 from algopy.utp.tests.test_utps import *
-from algopy.utp.tests.test_utpm import *
+from algopy.utp.utpm.tests.test_utpm import *
+
+from algopy.tests.test_globalfuncs import *
 
 
 if __name__ == "__main__":
