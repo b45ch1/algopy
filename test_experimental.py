b45ea5e03e335fcff8efcb319b5cdc3bf29a1e24
Sebastian <b45ch1@shlp.(none)>
2010-03-22 00:29:24 +0100

    played around with the QR decomposition with near singular A

--- test_experimental.py
+++ test_experimental.py
@@ -34,9 +34,71 @@ class Test_Experimental(TestCase):
             # assert_almost_equal(numpy.trace(numpy.dot(Ab.T,Ad)), numpy.trace(numpy.dot(Lb.T,Ld) ))
 
 
-
-
-
+    # def test_something(self):
+        
+    #     D,P,N = 3, 1, 3
+    #     tmp = numpy.zeros((D,P,N,N))
+    #     tmp[0,0] = [[0,0,0],[0,0,0],[0,1,0]]
+    #     A = UTPM(tmp)
+    #     print A
+    pass
+        
 
 if __name__ == "__main__":
-    run_module_suite() 
+    # run_module_suite() 
+    from adolc import adouble
+    from adolc.cgraph import AdolcProgram
+    from adolc.linalg import qr
+    D,P,N = 2, 1, 3
+    
+    A_data = numpy.random.rand(D,P,N,N)
+    A_data[0,0] = numpy.array([[1.,0,0],[0,1.,-1],[0,-1,1.]])
+    
+    #----------------------------------------------
+    # STEP 1:
+    # QR decomposition by Givens Rotations
+    # using pyadolc for the differentiation
+    #----------------------------------------------
+    A = A_data[0,0,:,:]
+    
+    # trace QR decomposition with adolc
+    AP = AdolcProgram()
+    AP.trace_on(1)
+    aA = adouble(A)
+    AP.independent(aA)
+    aQ, aR = qr(aA)
+    AP.dependent(aQ)
+    AP.dependent(aR)
+    AP.trace_off()
+    
+    # compute push forward
+    VA = A_data[1:,...].transpose((2,3,1,0))
+    # print VA
+    out = AP.forward([A],[VA])
+    
+    Q_data = numpy.zeros((D,P,N,N))
+    R_data = numpy.zeros((D,P,N,N))
+    
+    Q_data[0,0] = out[0][0]
+    R_data[0,0] = out[0][1]
+    
+    Q_data[1:,0] = out[1][0][:,:,0,:].transpose((2,0,1))
+    R_data[1:,0] = out[1][1][:,:,0,:].transpose((2,0,1))
+    
+    Q = UTPM(Q_data)
+    R = UTPM(R_data)
+    
+    A = UTPM(A_data)
+    print A - UTPM.dot(Q,R)
+    
+    print numpy.rank(A_data[0,0])
+    
+    Q,R = UTPM.qr(A[:,:])
+    
+    
+    print A - UTPM.dot(Q,R)
+
+    
+    
+    
+    
