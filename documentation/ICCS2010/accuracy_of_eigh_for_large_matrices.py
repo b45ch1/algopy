de41b162019bed21a1a408e9a007371302abec48
Sebastian <b45ch1@shlp.(none)>
2010-05-02 16:25:19 +0200

    added example that asserts correctness of eigh for large matrices and large degree

--- documentation/ICCS2010/accuracy_of_eigh_for_large_matrices.py
+++ documentation/ICCS2010/accuracy_of_eigh_for_large_matrices.py
@@ -4,9 +4,7 @@ import numpy
 from algopy.tracer.tracer import *
 from algopy.utp.utpm import *
 
-
 D,P,N = 4,1,100
-
 A = UTPM(numpy.random.rand(D,P,N,N))
 Q,R = UTPM.qr(A)
 l = UTPM(numpy.random.rand(D,P,N))
@@ -16,15 +14,8 @@ A = UTPM.dot(Q, UTPM.dot(L,Q.T))
 
 l2,Q2 = UTPM.eigh(A)
 
+print 'error between true and reconstructed eigenvalues'
 print l.data[:,:,numpy.argsort(l.data[0,0])] - l2.data
 
 
 
-if __name__ == "__main__":
-    run_module_suite() 
-
-
-    
-    
-    
-    
