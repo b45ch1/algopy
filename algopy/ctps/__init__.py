d5acdc01dde5b397dabe5553affc4f11dee79aa6
Sebastian F. Walter <sebastian.walter@gmail.com>
2010-05-11 17:58:40 +0200

    added deprecation warnings to the ctps and utps init files

--- algopy/ctps/__init__.py
+++ algopy/ctps/__init__.py
@@ -1,1 +1,3 @@
 from ctps import *
+
+print 'The submodule algopy.utps is deprecated and tagged for removal'
--- algopy/utps/__init__.py
+++ algopy/utps/__init__.py
@@ -1,1 +1,3 @@
 from utps import *
+
+print 'The submodule algopy.utps is deprecated and tagged for removal'
