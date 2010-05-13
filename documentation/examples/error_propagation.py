97ca5bf0a66086a8657058f346b540196a90b008
Sebastian Walter <sebastian.walter@gmail.com>
2010-05-13 18:32:40 +0200

    improved the error propagation example a little, moved an example to the examples folder

--- documentation/examples/error_propagation.py
+++ documentation/examples/error_propagation.py
@@ -9,12 +9,7 @@ where x,y,\epsilon arrays of size Nm. \epsilon is a iid normally distributed
 random variable with mean zero and covariance matrix \Sigma^2.
 The y are the observed quantities and x a real vector.
 
-The question is:
-
-    What can we say about the confidence region of a function f(y) when
-    the confidence region of y is described by the covariance matrix \Sigma^2?
-
-The idea is to compute the Jacobian J(\hat x) of a function::
+However, not y is of interest by some function f(y)::
 
     f: R^Nm ---> R^M
         y  ---> z = f(y)
@@ -22,11 +17,26 @@ The idea is to compute the Jacobian J(\hat x) of a function::
 Since y depends on the random variable \epsilon it is also a random variable 
 with the same covariance matrix as \epsilon.
 
-The covariance matrix of z, i.e. E[z z^T] = E[ J y y J^T] = J \Sigma^2 J^T.
-I.e., if we know J(y) we can approximately compute the confidence region if
-f(y) is sufficiently linear.
+The question is:
 
-In the forward mode of AD we compute Nm directional derivatives, i.e. P = Nm.
+    What can we say about the confidence region of the function f(y) when
+    the confidence region of y is described by the covariance matrix \Sigma^2?
+
+For affine (linear) functions f(y) = Ay + b the procedure is described in the 
+wikipedia article http://en.wikipedia.org/wiki/Propagation_of_uncertainty .
+
+For nonlinear functions can be linearized about an estimate \hat y of E[y].
+In the vicinity of \hat y, the linear model approximates the nonlinear function often quite well.
+
+To linearize the function, the Jacobian J(\hat y) of the function f(\hat y) has to be computed.
+
+The covariance matrix of z is defined as C = E[z z^T] = E[ J y y^T J^T] = J \Sigma^2 J^T.
+That means if we know J(y), we can approximately compute the confidence region if
+f(\hat y) is sufficiently linear.
+
+To compute the Jacobian one can use the forward and the reverse mode of AD:
+In the forward mode of AD one computes Nm directional derivatives, i.e. P = Nm.
+In the reverse mode of AD one computes M adjoint derivatives, i.e. Q = M.
 """
 
 import numpy
@@ -47,11 +57,8 @@ y.data[0,:] = numpy.random.rand(Nm)
 y.data[1,:] = numpy.eye(Nm)
 
 
-
 # print f(y)
-
 J = f(y).data[1,:,:,0]
-
 print 'Jacobian J(y) = \n', J
 
 C_epsilon = 0.3*numpy.eye(Nm)
