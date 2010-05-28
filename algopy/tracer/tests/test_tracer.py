2fae49a63ff0f0c946b21063fc2b94d8720f29c4
Sebastian Walter <sebastian.walter@gmail.com>
2010-05-28 16:31:41 +0200

    added another test of reverse mode with intrinsic QR decomposition: compared gradient computed in the forward mode with the gradient computed in the reverse mode

--- algopy/tracer/tests/test_tracer.py
+++ algopy/tracer/tests/test_tracer.py
@@ -7,6 +7,8 @@ from algopy.utpm import UTPM
 from algopy.utps import UTPS
 from algopy.ctps import CTPS
 
+from algopy import dot, eigh, qr, trace, solve
+
 
 import numpy
 
@@ -1031,6 +1033,36 @@ class Test_CGgraph_on_UTPM(TestCase):
         cg.push_forward([x])
         assert_array_almost_equal(Fy.x.data[0], x.data[0])
 
+
+    def test_pullback_gradient(self):
+        (D,P,M,N) = 2,9,3,3
+        A = UTPM(numpy.zeros((D,P,M,N)))
+        
+        for m in range(M):
+            for n in range(N):
+                p = m*N + n
+                A.data[0,p] = numpy.random.rand(M,N)
+                A.data[1,p,m,n] = 1.
+        
+        cg = CGraph()
+        A = Function(A)        
+        Q,R = qr(A)
+        B = dot(Q,R)
+        y = trace(B)
+        
+        cg.independentFunctionList = [A]
+        cg.dependentFunctionList = [y]
+        
+        # print cg
+        
+        g1 =  y.x.data[1]
+        ybar = y.x.zeros_like()
+        ybar.data[0,0] = 1.
+        cg.pullback([ybar])
+        
+        g2 = A.xbar.data[0,0].ravel()
+        
+        assert_array_almost_equal(g1, g2)
 
 
 class TestCGraphOnCTPS(TestCase):
