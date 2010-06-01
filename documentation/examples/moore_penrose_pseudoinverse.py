258f2310e7ea777cff6e96c177c12cc5a3ca02c5
Sebastian Walter <sebastian.walter@gmail.com>
2010-06-01 10:44:14 +0200

    extended example on the moore_penrose_inverse computatoin

--- documentation/examples/moore_penrose_pseudoinverse.py
+++ documentation/examples/moore_penrose_pseudoinverse.py
@@ -1,12 +1,20 @@
 """
-In this example it is the goal to compute derivatives of the Moore-Penrose pseudoinverse
-in a stable manner.
+In this example it is the goal to compute _derivatives_ of the Moore-Penrose pseudoinverse
+in a stable manner. We compare different possibilities:
 
-I.e. compute::
+    1) A naive approach where A^T A is explicitly computed (numerically unstable)
+    2) A QR approach where at first a QR decomposition of A is formed and the inverse
+       is computed by a forward and then back substition of R. This method uses
+       the differentiated QR decomposition.
+    3) A direct approach where an analytic formula for the derivatives of the 
+       Moore-Penrose Formula is derived. Then usage of the QR decomposition is 
+       used to make the computation of the analytical formula numerically stable.
+
+More explicitly, we compute::
 
     A^\dagger = (A^T A)^{-1} A^T
 
-where A is a (M,N) array (M >= N) with possibly bad condition number
+where A is a (M,N) array (M >= N) with possibly bad condition number.
 """
 
 import numpy
@@ -15,11 +23,9 @@ from algopy import CGraph, Function, UTPM, dot, qr, eigh, inv, solve
 # first order derivatives, one directional derivative
 # D - 1 is the degree of the Taylor polynomial
 # P directional derivatives at once
-# M number of rows of J1
-# N number of cols of J1
-# K number of rows of J2 (must be smaller than N)
-D,P,M,N,K,Nx = 2,1,5,2,1,1
-
+# M number of rows of A
+# N number of cols of A
+D,P,M,N = 2,1,5,2
 
 # generate badly conditioned matrix A
 A = UTPM(numpy.zeros((D,P,M,N)))
@@ -32,11 +38,13 @@ x.data[1,0,:,0] = [1,1,1,1,1]
 y.data[0,0,:,0] = [1,2,1,2,1]
 y.data[1,0,:,0] = [1,2,1,2,1]
 
-
 alpha = 10**-5
 A = dot(x,x.T) + alpha*dot(y,y.T)
 
 A = A[:,:2]
+
+
+# Method 1: Naive approach
 Apinv = dot(inv(dot(A.T,A)),A.T)
 
 print 'naive approach: A Apinv A - A = 0 \n', dot(dot(A, Apinv),A) - A
@@ -44,8 +52,8 @@ print 'naive approach: Apinv A Apinv - Apinv = 0 \n', dot(dot(Apinv, A),Apinv) -
 print 'naive approach: (Apinv A)^T - Apinv A = 0 \n', dot(Apinv, A).T  - dot(Apinv, A)
 print 'naive approach: (A Apinv)^T - A Apinv = 0 \n', dot(A, Apinv).T  - dot(A, Apinv)
 
-# print dot(C,B)
 
+# Method 2: Using the differentiated QR decomposition
 Q,R = qr(A)
 tmp1 = solve(R.T, A.T)
 tmp2 = solve(R, tmp1)
@@ -56,8 +64,26 @@ print 'QR approach: Apinv A Apinv - Apinv = 0 \n', dot(dot(Apinv, A),Apinv) - Ap
 print 'QR approach: (Apinv A)^T - Apinv A = 0 \n', dot(Apinv, A).T  - dot(Apinv, A)
 print 'QR approach: (A Apinv)^T - A Apinv = 0 \n', dot(A, Apinv).T  - dot(A, Apinv)
 
+# Method 3: Stable evaluation of the analytical derivative formula
 
-# print numpy.linalg.svd(Apinv.data[0,0])[1]
+A0 = A.data[0,0]
+A1 = A.data[1,0]
 
-# print 'dot(A, Apinv)=',dot(A, Apinv)
+Q0, R0 = numpy.linalg.qr(A0)
 
+# compute nominal solution
+tmp1 = solve(R0.T, A0.T)
+C0 = solve(R0, tmp1)
+
+# compute first directional derivative
+tmp2 = A1.T - dot( dot(A1.T, A0) + dot(A0.T, A1), C0)
+tmp1 = solve(R0.T, tmp2)
+C1 = solve(R0, tmp1)
+
+Apinv.data[0,0] = C0
+Apinv.data[1,0] = C1
+
+print 'analytical approach: A Apinv A - A = 0 \n',  dot(dot(A, Apinv),A) - A
+print 'analytical approach: Apinv A Apinv - Apinv = 0 \n', dot(dot(Apinv, A),Apinv) - Apinv
+print 'analytical approach: (Apinv A)^T - Apinv A = 0 \n', dot(Apinv, A).T  - dot(Apinv, A)
+print 'analytical approach: (A Apinv)^T - A Apinv = 0 \n', dot(A, Apinv).T  - dot(A, Apinv)
