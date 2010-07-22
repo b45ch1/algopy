e3b325e9ac9ca3be60932ee97c11f32004624303
Sebastian Walter <sebastian.walter@gmail.com>
2010-07-22 23:58:10 +0200

    accidentally used TAYLORPOLY in the explict euler example, fixed that now

--- documentation/sphinx/examples/explicit_euler.py
+++ documentation/sphinx/examples/explicit_euler.py
@@ -1,17 +1,24 @@
-import numpy; from numpy import sin,cos; from taylorpoly import UTPS
-x = numpy.array([UTPS([1,0,0],P=2), UTPS([0,0,0],P=2)])
-p = UTPS([3,1,0],P=2)
+import numpy; from numpy import sin,cos; from algopy import UTPM, zeros
+D,P = 2,1
+x = UTPM(numpy.zeros((D,P,2)))
+x.data[0,:,0] = 1
+p = UTPM(numpy.zeros((D,P)))
+p.data[0,:] = 3; p.data[1,:] = 1
+
 def f(x):
-    return numpy.array([x[1],-p * x[0]])
+    retval = x.zeros_like()
+    retval[0] = x[1]
+    retval[1] = -p* x[0]
+    return retval
 
 # compute AD solution
-ts = numpy.linspace(0,2*numpy.pi,3000)
-x_list = [[xi.data.copy() for xi in x]]
+ts = numpy.linspace(0,2*numpy.pi,2000)
+x_list = [x.data.copy() ]
 for nts in range(ts.size-1):
     h = ts[nts+1] - ts[nts]
     x = x + h * f(x)
-    x_list.append([xi.data.copy() for xi in x])
-    
+    x_list.append(x.data.copy())
+
 # analytical solution
 def x_analytical(t,p):
     return numpy.cos(numpy.sqrt(p)*t)
@@ -20,11 +27,12 @@ def x_p_analytical(t,p):
     return -0.5*numpy.sin(numpy.sqrt(p)*t)*p**(-0.5)*t
 
 xs = numpy.array(x_list)
+print xs.shape
 import matplotlib.pyplot as pyplot
-pyplot.plot(ts, xs[:,0,0], ',k-', label = r'$x(t)$')
-pyplot.plot(ts, x_analytical(ts,p.data[0]), 'k-.', label = r'analytic $x(t)$')
-pyplot.plot(ts, xs[:,0,1], ',r-', label = r'$x_p(t)$')
-pyplot.plot(ts, x_p_analytical(ts,p.data[0]), 'r-.', label = r'analytic $x_p(t)$')
+pyplot.plot(ts, xs[:,0,0,0], ',k-', label = r'$x(t)$')
+pyplot.plot(ts, x_analytical(ts,p.data[0,0]), 'k-.', label = r'analytic $x(t)$')
+pyplot.plot(ts, xs[:,1,0,0], ',r-', label = r'$x_p(t)$')
+pyplot.plot(ts, x_p_analytical(ts,p.data[0,0]), 'r-.', label = r'analytic $x_p(t)$')
 
 pyplot.xlabel('time $t$')
 pyplot.legend(loc='best')
