13e6a228e418f06b343f030cee38ba1233f34e3f
Sebastian Walter <sebastian.walter@gmail.com>
2010-07-27 16:57:56 +0200

    implicit euler works now

--- documentation/sphinx/examples/implicit_euler.py
+++ documentation/sphinx/examples/implicit_euler.py
@@ -20,36 +20,44 @@ def implicit_euler(f_fcn, x0, ts, p):
         """ implicit function to solve:  0 = F(x_new, x, t_new, t_old)"""
         return (t_new - t) * f_fcn(t_new, x_new, p) - x_new + x
         
+    def J_fcn(x_new, x, t_new, t, p):
+        """ computes the Jacobian of F_fcn
+        all inputs are double arrays
+        """
+        y = UTPM(numpy.zeros((D,N,N)))
+        y.data[0,:]   = x_new
+        y.data[1,:,:] = numpy.eye(N)
+        F = F_fcn(y, x, t_new, t, p)
+        return F.data[1,:,:].T
         
-   
+        
     x = x0.copy()
     D,P,N = x.data.shape
-    y = UTPM(numpy.zeros((D,N,N)))
     x_new = x.copy()
 
     x_list = [x.data.copy() ]
     for nts in range(ts.size-1):
         h = ts[nts+1] - ts[nts]
-        
-        # compute the Jacobian
-        y.data[0,:]   = x.data[0,:]
-        y.data[1,:,:] = numpy.eye(N)
-        F = F_fcn(y, x.data[0,0], ts[nts+1], ts[nts], p.data[0,0])
-        J = F.data[1,:,:].T
+        x_new.data[0,...] = x.data[0,...]
+        x_new.data[1:,...] = 0
+
+        # compute the Jacobian at x
+        J = J_fcn(x_new.data[0,0], x.data[0,0], ts[nts+1], ts[nts], p.data[0,0])
 
         # d=0: apply Newton's method to solve 0 = F_fcn(x_new, x, t_new, t)
-        x_new[...] = x[...]
-        rel_error = numpy.inf
-        while rel_error > 10**-6:
+        step = numpy.inf
+        while step > 10**-10:
             delta_x = numpy.linalg.solve(J, F_fcn(x_new.data[0,0], x.data[0,0], ts[nts+1], ts[nts], p.data[0,0]))
             x_new.data[0,0] -= delta_x
-            rel_error = numpy.linalg.norm(delta_x)
+            step = numpy.linalg.norm(delta_x)
         
-        # # d>0: compute higher order coefficients
-        # for d in range(1,D):
-        #     x_new.data[d,0] = numpy.linalg.solve
+        # d>0: compute higher order coefficients
+        J = J_fcn(x_new.data[0,0], x.data[0,0], ts[nts+1], ts[nts], p.data[0,0])
+        for d in range(1,D):
+            F = F_fcn(x_new, x, ts[nts+1], ts[nts], p)
+            x_new.data[d,0] = -numpy.linalg.solve(J, F.data[d,0])
         
-        x[...] = x_new[...]
+        x.data[...] = x_new.data[...]
         x_list.append(x.data.copy())
         
     return numpy.array(x_list)
@@ -59,7 +67,7 @@ def implicit_euler(f_fcn, x0, ts, p):
 ts = numpy.linspace(0,2*numpy.pi,1000)
 xs = implicit_euler(f, x, ts, p)
 
-print xs
+# print xs
 # analytical solution
 def x_analytical(t,p):
     return numpy.cos(numpy.sqrt(p)*t)
