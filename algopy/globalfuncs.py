7c344155c17170ecafe00356a8a4250959d1d337
Sebastian Walter <sebastian.walter@iwr.uni-heidelberg.de>
2012-09-27 16:21:59 +0200

    added support for the SVD

--- algopy/globalfuncs.py
+++ algopy/globalfuncs.py
@@ -24,7 +24,7 @@ def $function_name(*args):
             case = 1
             arg  = na
             break
-            
+
     if case==1:
         return getattr(args[arg].__class__, '$function_name')(*args)
 
@@ -37,37 +37,37 @@ def $function_name(*args):
 
 for function_name in numpy_function_names:
     exec function_template.substitute(function_name=function_name, namespace='numpy')
-    
+
 for function_name in numpy_linalg_function_names:
     exec function_template.substitute(function_name=function_name, namespace='numpy.linalg')
 
 
 def sum(x, axis=None, dtype=None, out=None):
-    """ generic sum function 
+    """ generic sum function
     calls either numpy.sum or Function.sum resp. UTPM.sum depending on
     the input
     """
-    
+
     if isinstance(x, numpy.ndarray) or numpy.isscalar(x):
         return numpy.sum(x, axis=axis, dtype=dtype, out = out)
 
     elif isinstance(x, UTPM) or isinstance(x, Function):
        return x.sum(axis = axis, dtype = dtype, out = out)
-        
+
     else:
         raise ValueError('don\'t know what to do with this input!')
-        
-        
+
+
 def prod(x, axis=None, dtype=None, out=None):
-    """ generic prod function 
+    """ generic prod function
     """
-    
+
     if axis != None or dtype != None or out != None:
         raise NotImplementedError('')
-        
+
     elif isinstance(x, numpy.ndarray):
         return numpy.prod(x)
-        
+
     elif isinstance(x, Function) or  isinstance(x, UTPM):
         y = zeros(1,dtype=x)
         y[0] = x[0]
@@ -78,48 +78,48 @@ def prod(x, axis=None, dtype=None, out=None):
 
 def coeff_op(x, sl, shp):
     return x.coeff_op(sl, shp)
-    
 
-    
+
+
 def init_UTPM_jacobian(x):
     # print 'type(x)=', type(x)
     if isinstance(x, Function):
         return x.init_UTPM_jacobian()
-        
+
     elif isinstance(x, numpy.ndarray):
         return UTPM.init_jacobian(x)
-        
+
     elif isinstance(x, UTPM):
         # print x.data.shape
         return UTPM.init_UTPM_jacobian(x.data[0,0])
-        
+
     else:
-        raise ValueError('don\'t know what to do with this input!')    
-        
+        raise ValueError('don\'t know what to do with this input!')
+
 def extract_UTPM_jacobian(x):
     if isinstance(x, Function):
         return x.extract_UTPM_jacobian()
-        
+
     elif isinstance(x, UTPM):
-        return UTPM.extract_UTPM_jacobian(x)        
+        return UTPM.extract_UTPM_jacobian(x)
     else:
-        raise ValueError('don\'t know what to do with this input!')           
-        
-        
+        raise ValueError('don\'t know what to do with this input!')
+
+
 
 def zeros( shape, dtype=float, order = 'C'):
     """
     generic generalization of numpy.zeros
-    
+
     create a zero instance
     """
-    
+
     if numpy.isscalar(shape):
         shape = (shape,)
-        
+
     if isinstance(dtype,type):
         return numpy.zeros(shape, dtype=dtype,order=order)
-                
+
     elif isinstance(dtype, numpy.ndarray):
         return numpy.zeros(shape,dtype=dtype.dtype, order=order)
 
@@ -128,12 +128,12 @@ def zeros( shape, dtype=float, order = 'C'):
         tmp = numpy.zeros((D,P) + shape ,dtype = dtype.data.dtype)
         tmp*= dtype.data.flatten()[0]
         return dtype.__class__(tmp)
-        
+
     elif isinstance(dtype, Function):
         # dtype.create(zeros(shape, dtype=dtype.x, order = order), fargs, zeros):
         return dtype.pushforward(zeros, [shape, dtype, order])
         # return dtype.__class__(zeros(shape, dtype=dtype.x, order = order))
-        
+
     else:
         return numpy.zeros(shape,dtype=type(dtype), order=order)
         # raise ValueError('don\'t know what to do with dtype = %s, type(dtype)=%s'%(str(dtype), str(type(dtype))))
@@ -144,86 +144,145 @@ def dot(a,b):
     """
     if isinstance(a,Function) or isinstance(b,Function):
         return Function.dot(a,b)
-        
+
     elif isinstance(a,UTPM) or isinstance(b,UTPM):
         return UTPM.dot(a,b)
-        
+
     else:
         return numpy.dot(a,b)
-    
+
 def outer(a,b):
     """
     Same as NumPy outer but in UTP arithmetic
     """
     if isinstance(a,Function) or isinstance(b,Function):
         return Function.outer(a,b)
-        
+
     elif isinstance(a,UTPM) or isinstance(b,UTPM):
         return UTPM.outer(a,b)
-        
+
     else:
         return numpy.outer(a,b)
 
 
 
 def qr_full(A):
-    """ Q,R = qr_full(A) returns QR decomposition with quadratic Q 
+    """ Q,R = qr_full(A) returns QR decomposition with quadratic Q
     calls internally UTPM.qr_full or Function.qr_full
     """
     if isinstance(A, UTPM):
         return UTPM.qr_full(A)
-    
+
     elif isinstance(A, Function):
         return Function.qr_full(A)
-        
+
     elif isinstance(A, numpy.ndarray):
         return scipy.linalg.qr(A)
-        
+
     else:
         raise NotImplementedError('don\'t know what to do with this instance')
-        
-        
+
+
 def eigh1(A):
     if isinstance(A, UTPM):
         return UTPM.eigh1(A)
-    
+
     elif isinstance(A, Function):
         return Function.eigh1(A)
-        
+
     elif isinstance(A, numpy.ndarray):
         A = UTPM(A.reshape((1,1) + A.shape))
         retval = UTPM.eigh1(A)
         return retval[0].data[0,0], retval[1].data[0,0],retval[2]
-        
+
     else:
         raise NotImplementedError('don\'t know what to do with this instance')
-        
+
 def symvec(A, UPLO='F'):
     if isinstance(A, UTPM):
         return UTPM.symvec(A, UPLO=UPLO)
-    
+
     elif isinstance(A, Function):
         return Function.symvec(A, UPLO=UPLO)
-        
+
     elif isinstance(A, numpy.ndarray):
         return utils.symvec(A, UPLO=UPLO)
-        
+
     else:
         raise NotImplementedError('don\'t know what to do with this instance')
 symvec.__doc__ = utils.symvec.__doc__
-        
+
 def vecsym(v):
     if isinstance(v, UTPM):
         return UTPM.vecsym(v)
-    
+
     elif isinstance(v, Function):
         return Function.vecsym(v)
-        
+
     elif isinstance(v, numpy.ndarray):
         return utils.vecsym(v)
-        
+
     else:
         raise NotImplementedError('don\'t know what to do with this instance')
 vecsym.__doc__ = utils.vecsym.__doc__
 
+
+def svd(A):
+    """
+    computes the singular value decomposition A = U S V.T
+
+    (U, S, VT) = UTPM.svd(A, out=None)
+
+    Implementation
+    --------------
+
+    The singular value decomposition is directly related to the symmetric
+    eigenvalue decomposition.
+
+
+    See A. Bjoerk, Numerical Methods for Least Squares Problems, SIAM, 1996
+    for the relation between SVD and symm. eigenvalue decomposition
+
+    and S. F. Walter, Structured Higher-Order Algorithmic Differentiation
+    in the Forward and Reverse Mode with Application in Optimum Experimental
+    Design, PhD thesis, 2011
+    for the Taylor polynomial arithmetic.
+
+    """
+
+    M,N = A.shape
+
+    if N > M:
+        raise NotImplementedError("A.shape = (M,N) and N > M is not supported (yet)")
+
+    # real symmetric eigenvalue decomposition
+
+    B = zeros((M+N, M+N),dtype=A)
+    B[:M,M:] = A
+    B[M:,:M] = A.T
+    l,Q = eigh(B)
+
+    # permutation matrix
+
+    tmp = [M+N-1 - i for i in range(N)] + [i for i in range(N)] + [N+i for i in range(M-N)]
+    P = numpy.eye(M+N)
+    P = P[tmp]
+
+    # bring Q into the required format
+
+    Q = dot(Q, P.T)
+
+
+    # find U S V.T
+
+    U = zeros((M,M), dtype=Q)
+    V = zeros((N,N), dtype=Q)
+
+    U[:,:N] = 2.**0.5*Q[:M,:N]
+    U[:,N:] = Q[:M, 2*N: N+M]
+    V[:,:N] = 2.**0.5*Q[M:,:N]
+
+    s = -l[:N]
+
+    return U, s, V
 
--- algopy/tests/test_globalfuncs.py
+++ algopy/tests/test_globalfuncs.py
@@ -9,75 +9,75 @@ class Test_Global_Functions_on_Numpy_instances(TestCase):
     def test_global_unary_function(self):
         x = numpy.ones((6,6))
         assert_array_almost_equal(trace(x), numpy.trace(x))
-        
+
     def test_global_binary_function(self):
         x = numpy.random.rand(*(3,4))
         y = numpy.random.rand(*(4,3))
         assert_array_almost_equal(dot(x,y),numpy.dot(x,y))
-        
+
     def test_zeros(self):
         N,M = 3,4
         x = numpy.zeros((1,1),dtype=float)
         y = zeros((N,M), dtype=x)
         assert_array_almost_equal(numpy.zeros((N,M)), y)
-        
+
     def test_global_zeros_like(self):
         x = numpy.random.rand(*(3,4))
         y = zeros_like(x)
         assert_array_almost_equal(numpy.zeros((3,4)),y)
-        
+
     def test_global_linalg(self):
         x = numpy.random.rand(5,5)
         assert_array_almost_equal(inv(x),numpy.linalg.inv(x))
-        
-        
+
+
 class Test_Global_Functions_on_UTPM_instances(TestCase):
     def test_global_unary_function(self):
         D,P,N = 3,4,5
         x = UTPM(numpy.ones((D,P,N,N)))
         assert_array_almost_equal(trace(x).data, N * numpy.ones((D,P)))
-        
+
     def test_global_binary_function(self):
         D,P,N,M = 3,4,5,6
         x = UTPM(numpy.random.rand(*(D,P,N,M)))
         y = UTPM(numpy.random.rand(*(D,P,M,N)))
         assert_array_almost_equal(dot(x,y).data,UTPM.dot(x,y).data)
-        
+
     def test_zeros(self):
         D,P,N,M = 3,4,5,6
         x = UTPM(numpy.random.rand(*(D,P)))
         y = zeros((N,M), dtype=x)
         assert_array_almost_equal(numpy.zeros((D,P,N,M)),y.data)
-        
-        
+
+
     def test_zeros_with_mpmath_instances_as_dtype(self):
         skiptest = False
-        try:    
+        try:
             import mpmath
-            
+
         except:
             skiptest = True
-            
+
         if skiptest == False:
             x = UTPM(numpy.array([[mpmath.mpf(3)]]))
             A = zeros((2,2),dtype=x)
             assert_equal( True, isinstance(A.data[0,0,0,0], mpmath.mpf))
 
-        
+
     def test_global_zeros_like(self):
         D,P,N,M = 3,4,5,6
         x = UTPM(numpy.random.rand(*(D,P,N,M)))
         y = zeros_like(x)
         assert_array_almost_equal(numpy.zeros((D,P,N,M)),y.data)
-        
+
     def test_global_linalg(self):
         D,P,N = 3,4,5
         x = UTPM(numpy.random.rand(D,P,N,N))
         assert_array_almost_equal(inv(x).data, UTPM.inv(x).data)
 
 class Test_global_functions(TestCase):
-  
- 
+
+
     def test_numpy_overrides(self):
 
         X = 2 * numpy.random.rand(2,2,2,2)
@@ -88,10 +88,10 @@ class Test_global_functions(TestCase):
 
         assert_array_almost_equal( UTPM.dot(AX,AY).data, dot(AX,AY).data)
         assert_array_almost_equal( UTPM.inv(AX).data,  inv(AX).data)
-        
+
         assert_array_almost_equal( UTPM.trace(AX).data,  trace(AX).data)
 
-    
+
     # def test_convert(self):
         # X1 = 2 * numpy.random.rand(2,2,2,2)
         # X2 = 2 * numpy.random.rand(2,2,2,2)
@@ -104,6 +104,21 @@ class Test_global_functions(TestCase):
         # AY = combine_blocks([[AX1,AX2],[AX3,AX4]])
 
         # assert_array_equal(numpy.shape(AY.data),(2,2,4,4))
+
+    def test_svd(self):
+        D,P,M,N = 3,1,5,2
+        A = UTPM(numpy.random.random((D,P,M,N)))
+
+        U,s,V = svd(A)
+
+        S = zeros((M,N),dtype=A)
+        S[:N,:N] = diag(s)
+
+        assert_array_almost_equal( (dot(dot(U, S), V.T) - A).data, 0.)
+        assert_array_almost_equal( (dot(U.T, U) - numpy.eye(M)).data, 0.)
+        assert_array_almost_equal( (dot(U, U.T) - numpy.eye(M)).data, 0.)
+        assert_array_almost_equal( (dot(V.T, V) - numpy.eye(N)).data, 0.)
+        assert_array_almost_equal( (dot(V, V.T) - numpy.eye(N)).data, 0.)
 
 
 if __name__ == "__main__":
