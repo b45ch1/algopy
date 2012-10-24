1e52f8c5eecfc0c1f450ad18aed5d4a29b3f59be
Sebastian Walter <sebastian.walter@gmail.com>
2012-10-24 20:59:32 +0200

    moved functions from special/compound.py to special/special.py

--- algopy/special/special.py
+++ algopy/special/special.py
@@ -111,4 +111,111 @@ def erf(x):
 erf.__doc__ += scipy.special.erf.__doc__
 
 
+def erfi(x):
+    """
+    generic implementation of
+
+    y = erfi(x)
+
+    x:      either a
+
+            * float
+            * numpy.ndarray
+            * algopy.UTPM
+            * algopy.Function
+
+            instance.
+
+
+    """
+
+    if hasattr(x.__class__, 'erfi'):
+        return x.__class__.erfi(x)
+    else:
+        #FIXME: scipy.special.erfi does not yet exist
+        #return scipy.special.erfi(x)
+        return 2 * x * scipy.special.hyp1f1(0.5, 1.5, x*x) / (
+                math.sqrt(math.pi))
+
+#FIXME: scipy.special.erfi does not yet exist
+#erfi.__doc__ += scipy.special.erfi.__doc__
+
+
+def dawsn(x):
+    """
+    generic implementation of
+
+    y = dawsn(x)
+
+    x:      either a
+
+            * float
+            * numpy.ndarray
+            * algopy.UTPM
+            * algopy.Function
+
+            instance.
+
+
+    """
+
+    if hasattr(x.__class__, 'dawsn'):
+        return x.__class__.dawsn(x)
+    else:
+        return scipy.special.dawsn(x)
+
+dawsn.__doc__ += scipy.special.dawsn.__doc__
+
+
+def logit(x):
+    """
+    generic implementation of
+
+    y = logit(x)
+
+    x:      either a
+
+            * float
+            * numpy.ndarray
+            * algopy.UTPM
+            * algopy.Function
+
+            instance.
+
+
+    """
+
+    if hasattr(x.__class__, 'logit'):
+        return x.__class__.logit(x)
+    else:
+        return scipy.special.logit(x)
+
+logit.__doc__ += scipy.special.logit.__doc__
+
+
+def expit(x):
+    """
+    generic implementation of
+
+    y = expit(x)
+
+    x:      either a
+
+            * float
+            * numpy.ndarray
+            * algopy.UTPM
+            * algopy.Function
+
+            instance.
+
+
+    """
+
+    if hasattr(x.__class__, 'expit'):
+        return x.__class__.expit(x)
+    else:
+        return scipy.special.expit(x)
+
+expit.__doc__ += scipy.special.expit.__doc__
+
 
