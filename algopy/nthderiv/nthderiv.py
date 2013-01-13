ee586084023aa43abacef83f09e27809a4bc8cd0
alex <argriffi@ncsu.edu>
2013-01-13 00:08:50 -0500

    more testing and bug fixing for the nthderiv branch

--- algopy/nthderiv/nthderiv.py
+++ algopy/nthderiv/nthderiv.py
@@ -19,15 +19,20 @@ import numpy as np
 import numpy.testing
 from numpy.testing import assert_allclose
 
-# fail later in the process, by raising import warnings instead of errors
 try:
     import scipy
     try:
         import scipy.special
     except ImportError as e:
-        raise ImportWarning(e)
+        pass
 except ImportError as e:
-    raise ImportWarning(e)
+    scipy = None
+
+try:
+    import mpmath
+except:
+    mpmath = None
+
 
 # Define some function domain constants for usage in testing.
 DOM_ALL = 'all real numbers'
@@ -47,9 +52,11 @@ _domain_constants = (
 
 # Define the names of functions and constants to export.
 __all__ = [
+
+        # these names are standard in numpy, scipy, or scipy.special
         'rint', 'fix', 'floor', 'ceil', 'trunc',
         'sign', 'absolute',
-        'erf', 'erfi', 'gammaln', 'psi', 'polygamma',
+        'hyperu', 'erf', 'erfi', 'gammaln', 'psi', 'polygamma',
         'exp', 'exp2', 'expm1',
         'log', 'log2', 'log10', 'log1p',
         'sqrt', 'square', 'negative', 'reciprocal',
@@ -57,6 +64,12 @@ __all__ = [
         'arcsin', 'arccos', 'arctan',
         'sinh', 'cosh', 'tanh',
         'arcsinh', 'arccosh', 'arctanh',
+        'hyp0f1', 'hyp1f1', 'hyp1f2', 'hyp2f0', 'hyp2f1', 'hyp3f0',
+
+        # this is a custom name
+        'hyp_pfq',
+
+        # these are custom constants
         'DOM_ALL', 'DOM_POS', 'DOM_GT_1', 'DOM_GT_NEG_1', 'DOM_ABS_LT_1',
         ]
 
@@ -66,10 +79,14 @@ __all__ = [
 # Python 3 would allow nicer function definitions.
 # http://stackoverflow.com/questions/5940180
 
-def basecase(fn_zeroth_deriv, domain=DOM_ALL):
+def basecase(fn_zeroth_deriv, domain=DOM_ALL, extras=0):
     """
     Deal with zeroth order derivatives.
-    Make some effort to describe the domain of the function.
+    Make some effort to describe the domain of the function
+    and weirdnesses of the function signature such as extra parameters.
+    @param fn_zeroth_deriv: this is the function called when n is zero
+    @param domain: a constant that gives a rough domain indication
+    @param extras: the number of extra parameters for example hyperu has two
     """
     if domain not in _domain_constants:
         raise ValueError
@@ -79,6 +96,8 @@ def basecase(fn_zeroth_deriv, domain=DOM_ALL):
             n = kwargs.pop('n', 0)
             if kwargs:
                 raise ValueError('unexpected keyword args: %s' % kwargs)
+            if n < 0:
+                raise ValueError('n must be a nonnegative integer')
             if n:
                 return f(*args, out=out, n=n)
             elif out is None:
@@ -88,14 +107,28 @@ def basecase(fn_zeroth_deriv, domain=DOM_ALL):
         wrapped_f.__name__ = fn_zeroth_deriv.__name__
         wrapped_f.__doc__ = fn_zeroth_deriv.__doc__
         wrapped_f.domain = domain
+        wrapped_f.extras = extras
         return wrapped_f
     return wrap
 
 
 ##############################################################################
-# These constants and functions are not defined in numpy or in scipy.
+# These constants and functions are either not defined in numpy or in scipy,
+# or they are defined in weird ways.
+# Some of the changes I am making here may have already been made in
+# development versions of scipy, so by the time you read this,
+# chances are that you can just use the library functions.
 
 np_sqrt_pi = np.sqrt(np.pi)
+
+def np_sec(x, out=None):
+    return np.reciprocal(np.cos(x), out)
+
+def np_cot(x, out=None):
+    return np.reciprocal(np.tan(x), out)
+
+def np_csc(x, out=None):
+    return np.reciprocal(np.sin(x), out)
 
 def np_sech(x, out=None):
     return np.reciprocal(np.cosh(x), out)
@@ -122,8 +155,70 @@ def np_filled_like(x, fill_value, out=None):
     out.fill(fill_value)
     return out
 
+def np_fix(x, out=None):
+    """
+    This is changed because the 'out' arg of np.fix is called 'y'.
+    """
+    return np.fix(x, out)
+
+def np_polygamma(m, x, out=None):
+    """
+    This is changed because scipy.special.polygamma does not have 'out'.
+    """
+    if out is None:
+        out = np.copy(x)
+    out[...] = scipy.special.polygamma(m, x)
+    return out
+
 def np_recip_sqrt(x, out=None):
     return np.reciprocal(scipy.sqrt(x), out)
+
+def np_hyp0f1(b, x, out=None):
+    # work around a couple of scipy.special.hyp0f1 failures in old versions
+    with np.errstate(invalid='ignore'):
+        return np_real(scipy.special.hyp0f1(b, x + 0j), out)
+
+def np_hyp1f2(a1, b1, b2, x, out_val=None, out_err=None):
+    # ignore the error return value to give a more uniform interface
+    out_val, out_err = scipy.special.hyp1f2(a1, b1, b2, x, out_val, out_err)
+    return out_val
+
+def np_hyp2f0(a1, a2, x, out_value=None, out_error=None):
+    # pick a convergence type arbitrarily
+    convergence_type = 2
+    out_value, out_error = scipy.special.hyp2f0(
+            a1, a2, x, convergence_type,
+            out_value, out_error)
+    return out_value
+
+def np_hyp3f0(a1, a2, a3, x, out_val=None, out_err=None):
+    # ignore the error return value to give a more uniform interface
+    out_val, out_err = scipy.special.hyp3f0(a1, a2, a3, x, out_val, out_err)
+    return out_val
+
+def np_hyp_pfq(A, B, x, out=None):
+    d = {
+            (0, 1) : np_hyp0f1,
+            (1, 1) : scipy.special.hyp1f1,
+            (1, 2) : np_hyp1f2,
+            (2, 0) : np_hyp2f0,
+            (2, 1) : scipy.special.hyp2f1,
+            (3, 0) : np_hyp3f0,
+            }
+    pq = len(A), len(B)
+    try:
+        fn = d[pq]
+    except KeyError:
+        raise ValueError('hyp%df%d is not supported' % pq)
+    args = A + B + [x] + [out]
+    return fn(*args)
+
+# FIXME: replace these with scipy.special.polylog when it is available
+if mpmath:
+    mpmath_polylog_real = np.vectorize(
+            mpmath.fp.polylog, otypes=[np.float64])
+    mpmath_polylog_complex = np.vectorize(
+            mpmath.fp.polylog, otypes=[np.complex128])
 
 
 ##############################################################################
@@ -133,7 +228,7 @@ def np_recip_sqrt(x, out=None):
 def rint(x, out=None, n=0):
     return np_filled_like(x, 0, out)
 
-@basecase(np.fix)
+@basecase(np_fix)
 def fix(x, out=None, n=0):
     return np_filled_like(x, 0, out)
 
@@ -164,39 +259,45 @@ def absolute(x, out=None, n=0):
 ##############################################################################
 # Miscellaneous special functions.
 
+@basecase(scipy.special.hyperu, extras=2)
+def hyperu(a, b, x, out=None, n=0):
+    out = scipy.special.hyperu(a+n, b+n, x, out)
+    out *= pow(-1, n) * scipy.special.poch(a, n)
+    return out
+
 @basecase(scipy.special.erf)
 def erf(x, out=None, n=0):
-    a = pow(2, -n) * math.factorial(n - 1) * np.reciprocal(np_sqrt_pi)
-    b = np.exp(-np.square(x))
-    c = np.zeros_like(x)
-    for k in range(1, n+1):
-        s_a = pow(-1, k-1) * pow(2, 2*k) * pow(x, 2*k - n - 1)
-        s_b = math.factorial(2*k - n - 1) * math.factorial(n - k)
-        c += s_a * s_b
-    return np.multiply(a*b, c, out)
+    a = 2 * np.reciprocal(np_sqrt_pi) * np.exp(-np.square(x))
+    b = np.zeros_like(x)
+    for k in range(n):
+        sa = pow(-1, k) * np.exp2(2*k + 1 - n) * pow(x, 2*k + 1 - n)
+        sb = scipy.special.poch(2*k + 2 - n, 2*(n - 1 - k))
+        sc = math.factorial(n - 1 - k)
+        b += (sa * sb) / sc
+    return np.multiply(a, b, out)
 
 @basecase(scipy.special.erfi)
 def erfi(x, out=None, n=0):
-    a = pow(2, -n) * math.factorial(n - 1) * np.reciprocal(np_sqrt_pi)
-    b = np.exp(np.square(x))
-    c = np.zeros_like(x)
-    for k in range(1, n+1):
-        s_a = pow(2, 2*k) * pow(x, 2*k - n - 1)
-        s_b = math.factorial(2*k - n - 1) * math.factorial(n - k)
-        c += s_a * s_b
-    return np.multiply(a*b, c, out)
+    a = 2 * np.reciprocal(np_sqrt_pi) * np.exp(np.square(x))
+    b = np.zeros_like(x)
+    for k in range(n):
+        sa = np.exp2(2*k + 1 - n) * pow(x, 2*k + 1 - n)
+        sb = scipy.special.poch(2*k + 2 - n, 2*(n - 1 - k))
+        sc = math.factorial(n - 1 - k)
+        b += (sa * sb) / sc
+    return np.multiply(a, b, out)
 
-@basecase(scipy.special.gammaln, DOM_POS)
+@basecase(scipy.special.gammaln, domain=DOM_POS)
 def gammaln(x, out=None, n=0):
-    return scipy.special.polygamma(n-1, x, out)
+    return np_polygamma(n-1, x, out)
 
 @basecase(scipy.special.psi)
 def psi(x, out=None, n=0):
-    return scipy.special.polygamma(n, x, out)
+    return np_polygamma(n, x, out)
 
-@basecase(scipy.special.polygamma)
+@basecase(np_polygamma, extras=1)
 def polygamma(m, x, out=None, n=0):
-    return scipy.special.polygamma(m+n, x, out)
+    return np_polygamma(m+n, x, out)
 
 
 ##############################################################################
@@ -216,31 +317,31 @@ def exp2(x, out=None, n=0):
 def expm1(x, out=None, n=0):
     return np.exp(x, out)
 
-@basecase(np.log, DOM_POS)
+@basecase(np.log, domain=DOM_POS)
 def log(x, out=None, n=0):
     out = np.power(x, -n, out)
     out *= pow(-1, n-1) * math.factorial(n-1)
     return out
 
-@basecase(np.log2, DOM_POS)
+@basecase(np.log2, domain=DOM_POS)
 def log2(x, out=None, n=0):
-    out = log(x, out, n)
+    out = log(x, out=out, n=n)
     out /= np.log(2)
     return out
 
-@basecase(np.log10, DOM_POS)
+@basecase(np.log10, domain=DOM_POS)
 def log10(x, out=None, n=0):
-    out = log(x, out, n)
+    out = log(x, out=out, n=n)
     out /= np.log(10)
     return out
 
-@basecase(np.log1p, DOM_GT_NEG_1)
+@basecase(np.log1p, domain=DOM_GT_NEG_1)
 def log1p(x, out=None, n=0):
     out = np.power(1 + x, -n, out)
     out *= pow(-1, n-1) * math.factorial(n-1)
     return out
 
-@basecase(np.sqrt, DOM_POS)
+@basecase(np.sqrt, domain=DOM_POS)
 def sqrt(x, out=None, n=0):
     out = np.power(x, 0.5 - n, out)
     out *= scipy.special.poch(1.5 - n, n)
@@ -282,26 +383,17 @@ def cos(x, out=None, n=0):
 
 @basecase(np.tan)
 def tan(x, out=None, n=0):
-    out = np_filled_like(x, 0, out)
-    if n == 1:
-        out += np.square(np.sec(x))
-    for k in range(n):
-        for j in range(k):
-            c = pow(np.cos(x), -2*k - 2)
-            s = np.sin(0.5 * n * np.pi + 2*(k - j)*x)
-            p = pow(-1, k) * pow(k-j, n-1) * pow(2, n - 2*k)
-            b = scipy.special.binom(n-1, k) * scipy.special.binom(2*k, j)
-            out += n * (c * s * p * b) / (k + 1)
-    return out
+    a = mpmath_polylog_complex(-n, -scipy.exp(-2j*x))
+    return np_real(a * pow(-2j, n+1), out)
 
-@basecase(np.arcsin, DOM_ABS_LT_1)
+@basecase(np.arcsin, domain=DOM_ABS_LT_1)
 def arcsin(x, out=None, n=0):
     x1 = np_recip_sqrt(1 - np.square(x))
     a = 1j * pow(-1j, n) * math.factorial(n-1) * pow(x1, n)
     b = scipy.special.eval_legendre(n-1, 1j * x * x1)
     return np_real(a*b, out)
 
-@basecase(np.arccos, DOM_ABS_LT_1)
+@basecase(np.arccos, domain=DOM_ABS_LT_1)
 def arccos(x, out=None, n=0):
     return np.negative(arcsin(x), out)
 
@@ -325,17 +417,8 @@ def cosh(x, out=None, n=0):
 
 @basecase(np.tanh)
 def tanh(x, out=None, n=0):
-    y = np.zeros_like(x, dtype=complex)
-    if n == 1:
-        y += np.square(np_sech(x))
-    for k in range(n):
-        for j in range(k):
-            c = pow(np.cosh(x), -2*k - 2)
-            s = np.sinh(0.5j * n * np.pi + 2*(k - j)*x)
-            p = pow(-1, k) * pow(k-j, n-1) * pow(2, n - 2*k)
-            b = scipy.special.binom(n-1, k) * scipy.special.binom(2*k, j)
-            y -= pow(1j, n) * n * (c * s * p * b) / (k + 1)
-    return np_real(y, out)
+    a = mpmath_polylog_real(-n, -np.exp(2*x))
+    return np.multiply(-np.exp2(n+1), a, out)
 
 @basecase(np.arcsinh)
 def arcsinh(x, out=None, n=0):
@@ -344,14 +427,14 @@ def arcsinh(x, out=None, n=0):
     b = scipy.special.eval_legendre(n-1, x * x1)
     return np.multiply(a, b, out)
 
-@basecase(np.arccosh, DOM_GT_1)
+@basecase(np.arccosh, domain=DOM_GT_1)
 def arccosh(x, out=None, n=0):
     x1 = np_recip_sqrt(1 - np.square(x))
     a = -1 * pow(-1j, n) * math.factorial(n-1) * pow(x1, n)
     b = scipy.special.eval_legendre(n-1, 1j * x * x1)
     return np_real(a*b, out)
 
-@basecase(np.arctanh, DOM_ABS_LT_1)
+@basecase(np.arctanh, domain=DOM_ABS_LT_1)
 def arctanh(x, out=None, n=0):
     out = np.add(pow(1 - x, -n), pow(-1, n-1) * pow(x+1, -n), out)
     out *= 0.5 * math.factorial(n-1)
@@ -359,22 +442,35 @@ def arctanh(x, out=None, n=0):
 
 
 ##############################################################################
-# Hypergeometric functions.
+# Generalized hypergeometric functions of the pFq type.
 
-@basecase(scipy.special.hyp0f1)
+@basecase(np_hyp_pfq, extras=2)
+def hyp_pfq(A, B, x, out=None, n=0):
+    out = np_hyp_pfq([a+n for a in A], [b+n for b in B], x, out)
+    out *= np.prod([scipy.special.poch(a, n) for a in A])
+    out /= np.prod([scipy.special.poch(b, n) for b in B])
+    return out
+
+@basecase(np_hyp0f1, extras=1)
 def hyp0f1(b, x, out=None, n=0):
-    out = scipy.special.hyp0f1(b+n, x, out)
-    out /= scipy.special.poch(b, n)
-    return out
+    return hyp_pfq([], [b], x, out, n)
 
-@basecase(scipy.special.hyp1f1)
+@basecase(scipy.special.hyp1f1, extras=2)
 def hyp1f1(a, b, x, out=None, n=0):
-    out = scipy.special.hyp1f1(a+n, b+n, x, out)
-    out *= scipy.special.poch(a, n) / scipy.special.poch(b, n)
-    return out
+    return hyp_pfq([a], [b], x, out, n)
 
-@basecase(scipy.special.hyperu)
-def hyperu(a, b, x, out=None, n=0):
-    out = scipy.special.hyperu(a+n, b+n, x, out)
-    out *= pow(-1, n) * scipy.special.poch(a, n)
-    return out
+@basecase(np_hyp1f2, extras=3)
+def hyp1f2(a, b1, b2, x, out=None, n=0):
+    return hyp_pfq([a], [b1, b2], x, out, n)
+
+@basecase(np_hyp2f0, extras=2)
+def hyp2f0(a1, a2, x, out=None, n=0):
+    return hyp_pfq([a1, a2], [], x, out, n)
+
+@basecase(scipy.special.hyp2f1, extras=3)
+def hyp2f1(a1, a2, b1, x, out=None, n=0):
+    return hyp_pfq([a1, a2], [b1], x, out, n)
+
+@basecase(np_hyp3f0, extras=3)
+def hyp3f0(a1, a2, a3, x, out=None, n=0):
+    return hyp_pfq([a1, a2, a3], [], x, out, n)
--- algopy/tests/test_nthderiv.py
+++ algopy/tests/test_nthderiv.py
@@ -1,16 +1,76 @@
+"""
+Note that some of the numdifftools documentation is out of date.
+
+To figure out the right way to use numdifftools I had to directly inspect
+http://code.google.com/p/numdifftools/source/browse/trunk/numdifftools/core.py
+Also note that comparisons that use relative tolerances,
+such as are used by assert_allclose by default,
+do not work well when you are comparing to zero.
+"""
+
 import warnings
 
-import numpy
+import numpy as np
 import numpy.testing
-from numpy.testing import assert_allclose
+from numpy.testing import assert_allclose, assert_equal
 
 from algopy import nthderiv
 
 try:
     import sympy
 except ImportError as e:
-    sympy = None
     raise ImportWarning(e)
+    sympy = None
+
+try:
+    import numdifftools
+except ImportError as e:
+    warnings.warn('some tests require the numdifftools package')
+    numdifftools = None
+
+
+def assert_allclose_or_small(a, b, rtol=1e-7, zerotol=1e-7):
+    if np.amax(np.abs(a)) > zerotol or np.amax(np.abs(b)) > zerotol:
+        numpy.testing.assert_allclose(a, b, rtol=rtol)
+
+
+class TestAuto(numpy.testing.TestCase):
+
+    def test_syntax(self):
+        for name, f in nthderiv.__dict__.items():
+            domain = getattr(f, 'domain', None)
+            extras = getattr(f, 'extras', None)
+            if extras != 0:
+                continue
+            if domain not in (nthderiv.DOM_ALL, nthderiv.DOM_POS):
+                continue
+            for x in (0.123, np.array([0.123, 0.2, 1.1], dtype=float)):
+                for n in range(4):
+                    ya = f(x, n=n)
+                    yb = np.empty_like(x)
+                    f(x, out=yb, n=n)
+                    assert_equal(ya, yb)
+
+    @numpy.testing.decorators.skipif(numdifftools is None)
+    def test_numdifftools(self):
+        for name, f in nthderiv.__dict__.items():
+            domain = getattr(f, 'domain', None)
+            extras = getattr(f, 'extras', None)
+            if extras != 0:
+                continue
+            if domain not in (nthderiv.DOM_ALL, nthderiv.DOM_POS):
+                continue
+            for x in (0.123, np.array([0.123, 0.2, 1.1], dtype=float)):
+                for n in range(1, 5):
+                    ya = f(x, n=n)
+                    yb = numdifftools.Derivative(f, n=n)(x)
+                    print
+                    print name
+                    print 'n:', n
+                    print 'x:', x
+                    print 'ya:', ya
+                    print 'yb:', yb
+                    assert_allclose_or_small(ya, yb, rtol=1e-3, zerotol=1e-3)
 
 
 class TestLog(numpy.testing.TestCase):
@@ -18,7 +78,7 @@ class TestLog(numpy.testing.TestCase):
     def test_log_n0(self):
         x = 0.123
         a = nthderiv.log(x, n=0)
-        b = numpy.log(x)
+        b = np.log(x)
         assert_allclose(a, b)
 
     def test_log_n1(self):
@@ -30,19 +90,19 @@ class TestLog(numpy.testing.TestCase):
     def test_log_n2(self):
         x = 0.123
         a = nthderiv.log(x, n=2)
-        b = -1 / numpy.square(x)
+        b = -1 / np.square(x)
         assert_allclose(a, b)
 
     def test_log_n3(self):
         x = 0.123
         a = nthderiv.log(x, n=3)
-        b = 2 / numpy.power(x, 3)
+        b = 2 / np.power(x, 3)
         assert_allclose(a, b)
 
     def test_log1p_n0(self):
         x = 0.123
         a = nthderiv.log1p(x, n=0)
-        b = numpy.log1p(x)
+        b = np.log1p(x)
         assert_allclose(a, b)
 
     def test_log1p_n1(self):
@@ -54,13 +114,13 @@ class TestLog(numpy.testing.TestCase):
     def test_log1p_n2(self):
         x = 0.123
         a = nthderiv.log1p(x, n=2)
-        b = -1 / numpy.square(x + 1)
+        b = -1 / np.square(x + 1)
         assert_allclose(a, b)
 
     def test_log1p_n3(self):
         x = 0.123
         a = nthderiv.log1p(x, n=3)
-        b = 2 / numpy.power(x + 1, 3)
+        b = 2 / np.power(x + 1, 3)
         assert_allclose(a, b)
 
 
@@ -70,25 +130,25 @@ class TestMisc(numpy.testing.TestCase):
     def test_reciprocal(self):
         x = 0.123
         a = nthderiv.reciprocal(x, n=0)
-        b = numpy.reciprocal(x)
+        b = np.reciprocal(x)
         assert_allclose(a, b)
 
     def test_cos(self):
         x = 0.123
         a = nthderiv.cos(x, n=0)
-        b = numpy.cos(x)
+        b = np.cos(x)
         assert_allclose(a, b)
 
     def test_tan(self):
         x = 0.123
         a = nthderiv.tan(x, n=0)
-        b = numpy.tan(x)
+        b = np.tan(x)
         assert_allclose(a, b)
 
     def test_arctan_n0(self):
         x = 0.123
         a = nthderiv.arctan(x, n=0)
-        b = numpy.arctan(x)
+        b = np.arctan(x)
         assert_allclose(a, b)
 
     def test_arctan_n1(self):
@@ -100,41 +160,32 @@ class TestMisc(numpy.testing.TestCase):
     def test_cosh(self):
         x = 0.123
         a = nthderiv.cosh(x, n=0)
-        b = numpy.cosh(x)
+        b = np.cosh(x)
         assert_allclose(a, b)
 
     def test_sqrt_n0(self):
         x = 0.123
         a = nthderiv.sqrt(x, n=0)
-        b = numpy.sqrt(x)
+        b = np.sqrt(x)
         assert_allclose(a, b)
 
     def test_sqrt_n1(self):
         x = 0.123
         a = nthderiv.sqrt(x, n=1)
-        b = 1 / (2 * numpy.sqrt(x))
+        b = 1 / (2 * np.sqrt(x))
         assert_allclose(a, b)
 
     def test_arccosh_n0(self):
         x = 2.345
         a = nthderiv.arccosh(x, n=0)
-        b = numpy.arccosh(x)
+        b = np.arccosh(x)
         assert_allclose(a, b)
 
     def test_arccosh_n1(self):
         x = 2.345
         a = nthderiv.arccosh(x, n=1)
-        b = 1 / numpy.sqrt(x*x - 1)
+        b = 1 / np.sqrt(x*x - 1)
         assert_allclose(a, b)
-
-    """
-    def test_arccosh_decoration(self):
-        print nthderiv.arccosh
-        print nthderiv.arccosh.__name__
-        print nthderiv.arccosh.__doc__
-        print nthderiv.arccosh.domain
-        raise Exception
-    """
 
 
 if __name__ == '__main__':
